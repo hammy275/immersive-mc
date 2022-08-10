a4921266e485217939b5456fd67bf69322da2e0d
hammy3502 <hammy275@gmail.com>
2022-08-10 18:27:07 -0400

    Fix Chest Bugginess

--- src/main/java/net/blf02/immersivemc/mixin/ContainerOpenersCounterMixin.java
+++ src/main/java/net/blf02/immersivemc/mixin/ContainerOpenersCounterMixin.java
@@ -2,19 +2,19 @@ package net.blf02.immersivemc.mixin;
 
 import net.blf02.immersivemc.server.ChestToOpenCount;
 import net.minecraft.core.BlockPos;
-import net.minecraft.world.level.BlockGetter;
-import net.minecraft.world.level.block.entity.ChestBlockEntity;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.entity.ContainerOpenersCounter;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(ChestBlockEntity.class)
-public class ChestBlockEntityMixin {
+@Mixin(ContainerOpenersCounter.class)
+public class ContainerOpenersCounterMixin {
 
     @Inject(method="getOpenCount",
     at=@At("RETURN"), cancellable = true)
-    private static void onGetOpenCount(BlockGetter blockGetter, BlockPos pos, CallbackInfoReturnable<Integer> cir) {
+    private void onGetOpenCount(Level level, BlockPos pos, CallbackInfoReturnable<Integer> cir) {
         cir.setReturnValue(cir.getReturnValue() +
                 ChestToOpenCount.chestImmersiveOpenCount.getOrDefault(pos, 0));
     }
