5f97a89b07dd6b2debf0cd149635431fa00b9be3
hammy3502 <hammy275@gmail.com>
2022-12-26 14:45:43 -0500

    Mod Menu Support

---
+++ fabric/src/main/java/net/blf02/immersivemc_fabric/ModMenuIntegration.java
@@ -0,0 +1,13 @@
+package net.blf02.immersivemc_fabric;
+
+import com.terraformersmc.modmenu.api.ConfigScreenFactory;
+import com.terraformersmc.modmenu.api.ModMenuApi;
+import net.blf02.immersivemc.client.config.screen.ConfigScreen;
+
+public class ModMenuIntegration implements ModMenuApi {
+
+    @Override
+    public ConfigScreenFactory<?> getModConfigScreenFactory() {
+        return ConfigScreen::new;
+    }
+}
