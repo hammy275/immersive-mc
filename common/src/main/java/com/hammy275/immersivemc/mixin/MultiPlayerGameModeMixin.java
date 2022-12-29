1f5e0fe1df38b90e0da7bf25f6df2f03fd4cd4d5
hammy3502 <hammy275@gmail.com>
2022-12-28 19:47:46 -0500

    Mixin to client as well for #140 (closes #140)

---
+++ common/src/main/java/com/hammy275/immersivemc/mixin/MultiPlayerGameModeMixin.java
@@ -0,0 +1,33 @@
+package com.hammy275.immersivemc.mixin;
+
+import com.hammy275.immersivemc.common.config.ActiveConfig;
+import com.hammy275.immersivemc.common.util.Util;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.multiplayer.MultiPlayerGameMode;
+import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.phys.HitResult;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+
+@Mixin(MultiPlayerGameMode.class)
+public class MultiPlayerGameModeMixin {
+
+    @Shadow
+    private Minecraft minecraft;
+
+    @ModifyVariable(method= "performUseItemOn(Lnet/minecraft/client/player/LocalPlayer;Lnet/minecraft/world/InteractionHand;Lnet/minecraft/world/phys/BlockHitResult;)Lnet/minecraft/world/InteractionResult;",
+            at=@At("STORE"), index = 9, ordinal = 1, print = true)
+    // Matches bl2
+    public boolean isCrouchingCondition(boolean value) {
+        if (!ActiveConfig.crouchBypassImmersion) return value;
+        HitResult rawResult = Minecraft.getInstance().hitResult;
+        if (rawResult != null && rawResult instanceof BlockHitResult result) {
+            if (Util.isHittingImmersive(result, Minecraft.getInstance().level)) {
+                return false;
+            }
+        }
+        return value;
+    }
+}
