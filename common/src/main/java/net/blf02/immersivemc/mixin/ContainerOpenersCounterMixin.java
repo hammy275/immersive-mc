344fcd9b0e42828f56a41fe90d5402dfec03d362
hammy3502 <hammy275@gmail.com>
2022-12-29 16:06:01 -0500

    Fix Reach Mixin Conflict (closes #143)

--- common/src/main/java/net/blf02/immersivemc/mixin/ContainerOpenersCounterMixin.java
+++ common/src/main/java/net/blf02/immersivemc/mixin/ContainerOpenersCounterMixin.java
@@ -4,18 +4,17 @@ import net.blf02.immersivemc.server.ChestToOpenCount;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.ContainerOpenersCounter;
+import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 @Mixin(ContainerOpenersCounter.class)
 public class ContainerOpenersCounterMixin {
 
-    @Inject(method="getOpenCount",
-    at=@At("RETURN"), cancellable = true)
-    private void onGetOpenCount(Level level, BlockPos pos, CallbackInfoReturnable<Integer> cir) {
-        cir.setReturnValue(cir.getReturnValue() +
-                ChestToOpenCount.chestImmersiveOpenCount.getOrDefault(pos, 0));
+    @ModifyVariable(method = "recheckOpeners(Lnet/minecraft/world/level/Level;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/block/state/BlockState;)V",
+    at = @At("STORE"), index = 4, ordinal = 0)
+    public int openCountI(int originalI, Level level, BlockPos blockPos, BlockState blockState) {
+        return originalI + ChestToOpenCount.chestImmersiveOpenCount.getOrDefault(blockPos, 0);
     }
 }
--- common/src/main/java/net/blf02/immersivemc/mixin/ServerPlayerGameModeMixin.java
+++ common/src/main/java/net/blf02/immersivemc/mixin/ServerPlayerGameModeMixin.java
@@ -1,44 +1,28 @@
 package net.blf02.immersivemc.mixin;
 
+
 import net.blf02.immersivemc.common.util.Util;
 import net.blf02.immersivemc.server.PlayerConfigs;
-import net.blf02.immersivemc.server.ServerMixinData;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.server.level.ServerPlayerGameMode;
 import net.minecraft.world.InteractionHand;
-import net.minecraft.world.InteractionResult;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.BlockHitResult;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(ServerPlayerGameMode.class)
 public class ServerPlayerGameModeMixin {
 
-    @Final
-    @Shadow
-    protected ServerPlayer player;
-
-    @Inject(method= "useItemOn(Lnet/minecraft/server/level/ServerPlayer;Lnet/minecraft/world/level/Level;Lnet/minecraft/world/item/ItemStack;Lnet/minecraft/world/InteractionHand;Lnet/minecraft/world/phys/BlockHitResult;)Lnet/minecraft/world/InteractionResult;",
-    at=@At("HEAD"))
-    public void headOfCrouchingCheck(ServerPlayer serverPlayer, Level level, ItemStack itemStack, InteractionHand interactionHand, BlockHitResult blockHitResult, CallbackInfoReturnable<InteractionResult> cir) {
-        ServerMixinData.results.put(serverPlayer.getUUID(), blockHitResult);
-    }
-
     @ModifyVariable(method= "useItemOn(Lnet/minecraft/server/level/ServerPlayer;Lnet/minecraft/world/level/Level;Lnet/minecraft/world/item/ItemStack;Lnet/minecraft/world/InteractionHand;Lnet/minecraft/world/phys/BlockHitResult;)Lnet/minecraft/world/InteractionResult;",
     at=@At("STORE"), index = 9, ordinal = 1)
     // Matches bl2
-    public boolean isCrouchingCondition(boolean value) {
-        if (!PlayerConfigs.getConfig(this.player).crouchBypassImmersion) return value;
-        BlockHitResult result = ServerMixinData.results.get(this.player.getUUID());
-        if (result != null) {
-            if (Util.isHittingImmersive(result, this.player.level)) {
+    public boolean isCrouchingCondition(boolean value, ServerPlayer serverPlayer, Level level, ItemStack itemStack, InteractionHand interactionHand, BlockHitResult blockHitResult) {
+        if (!PlayerConfigs.getConfig(serverPlayer).crouchBypassImmersion) return value;
+        if (blockHitResult != null) {
+            if (Util.isHittingImmersive(blockHitResult, serverPlayer.level)) {
                 return false;
             }
         }
