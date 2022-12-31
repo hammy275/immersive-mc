85dab9f0e01481c52405dc690bd9fb5f55adc917
hammy3502 <hammy275@gmail.com>
2022-12-31 14:09:03 -0500

    Config Screen Improvements + Wiki Link

--- common/src/main/java/net/blf02/immersivemc/client/config/screen/ConfigScreen.java
+++ common/src/main/java/net/blf02/immersivemc/client/config/screen/ConfigScreen.java
@@ -4,17 +4,23 @@ import com.mojang.blaze3d.vertex.PoseStack;
 import net.blf02.immersivemc.common.config.ActiveConfig;
 import net.blf02.immersivemc.common.config.ImmersiveMCConfig;
 import net.blf02.immersivemc.common.vr.VRPluginVerify;
+import net.minecraft.Util;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.components.Button;
+import net.minecraft.client.gui.screens.ConfirmLinkScreen;
 import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.network.chat.CommonComponents;
+import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.TranslatableComponent;
 
 public class ConfigScreen extends Screen {
 
     protected final Screen lastScreen;
 
-    protected static int BUTTON_WIDTH = 128;
+    protected static int BUTTON_WIDTH = 160;
     protected static int BUTTON_HEIGHT = 20;
+
+    protected static final String WIKI_URL = "https://hammy275.com/immersivemcwiki";
 
     public ConfigScreen(Screen screen) {
         super(new TranslatableComponent("screen.immersivemc.config.title"));
@@ -44,6 +50,18 @@ public class ConfigScreen extends Screen {
                 (button) -> Minecraft.getInstance().setScreen(new ImmersivesConfigScreen(this,
                         VRPluginVerify.hasAPI ?
                                 ImmersivesConfigScreen.ScreenType.BOTH : ImmersivesConfigScreen.ScreenType.NONVR))
+        ));
+
+        this.addRenderableWidget(new Button(
+                (this.width - BUTTON_WIDTH) / 2, this.height - 26 - 32,
+                BUTTON_WIDTH, BUTTON_HEIGHT, new TranslatableComponent("config.immersivemc.wiki_button"),
+                (button -> Minecraft.getInstance().setScreen(
+                        new ConfirmLinkScreen((clickedYes) -> {
+                            if (clickedYes) {
+                                Util.getPlatform().openUri(WIKI_URL);
+                            }
+                            Minecraft.getInstance().setScreen(this);
+                        }, WIKI_URL, true)))
         ));
 
         this.addRenderableWidget(new Button(
