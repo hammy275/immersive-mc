883bf0ba4961197cfe3773f92ead0612317f22c4
hammy3502 <hammy275@gmail.com>
2023-01-09 16:50:48 -0500

    Ranged Grab Button (closes #167)

--- common/src/main/java/com/hammy275/immersivemc/client/tracker/RangedGrabTrackerClient.java
+++ common/src/main/java/com/hammy275/immersivemc/client/tracker/RangedGrabTrackerClient.java
@@ -1,5 +1,6 @@
 package com.hammy275.immersivemc.client.tracker;
 
+import com.hammy275.immersivemc.ImmersiveMC;
 import com.hammy275.immersivemc.common.config.ActiveConfig;
 import com.hammy275.immersivemc.common.network.Network;
 import com.hammy275.immersivemc.common.network.packet.GrabItemPacket;
@@ -28,6 +29,7 @@ public class RangedGrabTrackerClient extends AbstractTracker {
 
     protected IVRData last = null;
     protected ItemEntity selected = null;
+    protected int cooldown = 0;
 
     public RangedGrabTrackerClient() {
         ClientTrackerInit.trackers.add(this);
@@ -35,45 +37,52 @@ public class RangedGrabTrackerClient extends AbstractTracker {
 
     @Override
     protected void tick(Player player) {
+        if (cooldown > 0) {
+            cooldown--;
+        }
         IVRPlayer vrPlayer = VRPlugin.API.getVRPlayer(player);
         IVRData controller = vrPlayer.getController0();
 
-        double dist = ActiveConfig.rangedGrabRange == -1 ?
-                Minecraft.getInstance().gameMode.getPickRange() : ActiveConfig.rangedGrabRange;
+        if (cooldown <= 0) {
+            double dist = ActiveConfig.rangedGrabRange == -1 ?
+                    Minecraft.getInstance().gameMode.getPickRange() : ActiveConfig.rangedGrabRange;
 
-        if (last != null) {
-            if (Minecraft.getInstance().options.keyAttack.isDown()) {
-                if (controller.position().y - last.position().y > threshold
-                        && selected != null) {
-                    Network.INSTANCE.sendToServer(new GrabItemPacket(selected));
+            if (last != null) {
+                if (Minecraft.getInstance().options.keyAttack.isDown() ||
+                        ImmersiveMC.RANGED_GRAB_KEY.isDown()) {
+                    boolean grabFromMove = controller.position().y - last.position().y > threshold && Minecraft.getInstance().options.keyAttack.isDown();
+                    boolean grabFromKey = ImmersiveMC.RANGED_GRAB_KEY.isDown();
+                    if ((grabFromKey || grabFromMove) && selected != null) {
+                        Network.INSTANCE.sendToServer(new GrabItemPacket(selected));
+                        selected = null;
+                    }
+                } else {
                     selected = null;
-                }
-            } else {
-                selected = null;
 
-                Vec3 start = controller.position();
-                Vec3 viewVec = controller.getLookAngle();
-                Vec3 end = start.add(viewVec.x * dist, viewVec.y * dist,
-                        viewVec.z * dist);
+                    Vec3 start = controller.position();
+                    Vec3 viewVec = controller.getLookAngle();
+                    Vec3 end = start.add(viewVec.x * dist, viewVec.y * dist,
+                            viewVec.z * dist);
 
-                List<Entity> ents = player.level.getEntities(player, player.getBoundingBox().inflate(10),
-                        (entity -> entity instanceof ItemEntity && Util.canPickUpItem((ItemEntity) entity, player)));
-                List<AABB> hitboxes = new LinkedList<>();
-                for (Entity ent : ents) {
-                    hitboxes.add(ent.getBoundingBox().inflate(1d/3d));
-                }
-                Optional<Integer> result = Util.rayTraceClosest(start, end, hitboxes.toArray(new AABB[0]));
+                    List<Entity> ents = player.level.getEntities(player, player.getBoundingBox().inflate(10),
+                            (entity -> entity instanceof ItemEntity && Util.canPickUpItem((ItemEntity) entity, player)));
+                    List<AABB> hitboxes = new LinkedList<>();
+                    for (Entity ent : ents) {
+                        hitboxes.add(ent.getBoundingBox().inflate(1d/3d));
+                    }
+                    Optional<Integer> result = Util.rayTraceClosest(start, end, hitboxes.toArray(new AABB[0]));
 
-                if (result.isPresent()) {
-                    selected = (ItemEntity) ents.get(result.get());
+                    if (result.isPresent()) {
+                        selected = (ItemEntity) ents.get(result.get());
+                    }
                 }
             }
-        }
 
-        if (selected != null) {
-            Vec3 pos = selected.position().add(0, 0.2, 0);
-            selected.level.addParticle(new DustParticleOptions(new Vector3f(0, 1, 1), 1),
-                    pos.x, pos.y, pos.z, 0.01, 0.01, 0.01);
+            if (selected != null) {
+                Vec3 pos = selected.position().add(0, 0.2, 0);
+                selected.level.addParticle(new DustParticleOptions(new Vector3f(0, 1, 1), 1),
+                        pos.x, pos.y, pos.z, 0.01, 0.01, 0.01);
+            }
         }
 
         last = controller;
