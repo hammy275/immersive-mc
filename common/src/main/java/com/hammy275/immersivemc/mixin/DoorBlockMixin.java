556d2a3c08e97bc6e1b58bc04383fc5b400cf674
hammy3502 <hammy275@gmail.com>
2023-01-09 16:29:49 -0500

    Door Noise (closes #158)

---
+++ common/src/main/java/com/hammy275/immersivemc/mixin/DoorBlockMixin.java
@@ -0,0 +1,15 @@
+package com.hammy275.immersivemc.mixin;
+
+import net.minecraft.world.level.block.DoorBlock;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Invoker;
+
+@Mixin(DoorBlock.class)
+public interface DoorBlockMixin {
+
+    @Invoker("getOpenSound")
+    public int getOpenSound();
+
+    @Invoker("getCloseSound")
+    public int getCloseSound();
+}
