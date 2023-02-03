8c38793755f2fac62d95c0f7f5d77489338d1b63
hammy3502 <hammy275@gmail.com>
2023-02-02 19:32:54 -0500

    Check Throwing for Reel

--- common/src/main/java/com/hammy275/immersivemc/common/network/packet/ReelFishPacket.java
+++ common/src/main/java/com/hammy275/immersivemc/common/network/packet/ReelFishPacket.java
@@ -1,5 +1,6 @@
 package com.hammy275.immersivemc.common.network.packet;
 
+import com.hammy275.immersivemc.common.config.ActiveConfig;
 import dev.architectury.networking.NetworkManager;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.server.level.ServerPlayer;
@@ -25,7 +26,8 @@ public class ReelFishPacket {
         ctx.get().queue(() -> {
             ServerPlayer player = ctx.get().getPlayer() instanceof ServerPlayer ? (ServerPlayer) ctx.get().getPlayer() : null;
             // No need to VR check here. We're just doing the same thing as a right click, so this is safe as-is.
-            if (player != null && player.fishing != null && player.getItemInHand(InteractionHand.MAIN_HAND).getItem() instanceof FishingRodItem rod) {
+            if (ActiveConfig.useThrowing && player != null && player.fishing != null &&
+                    player.getItemInHand(InteractionHand.MAIN_HAND).getItem() instanceof FishingRodItem rod) {
                 rod.use(player.level, player, InteractionHand.MAIN_HAND);
             }
         });
