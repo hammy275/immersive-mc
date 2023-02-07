3571cbdfdce4f17634085b74fac0199b61dc203b
hammy3502 <hammy275@gmail.com>
2023-02-06 22:43:14 -0500

    Tooltip Alignment for Bottom Color Preview

--- common/src/main/java/com/hammy275/immersivemc/client/config/screen/ItemGuideCustomizeScreen.java
+++ common/src/main/java/com/hammy275/immersivemc/client/config/screen/ItemGuideCustomizeScreen.java
@@ -72,8 +72,10 @@ public class ItemGuideCustomizeScreen extends Screen {
                     mouseX, mouseY);
         }
 
+        RGBA color = ActiveConfig.rangedGrabColor;
+
         if (ScreenUtils.mouseInBox(mouseX, mouseY, this.width * 7 / 8 - 16,
-                this.height * 3 / 4 - 16, this.width * 7 / 8 + 16, this.height * 3 / 4 + 16)) {
+                this.height * 3 / 4 - 16 + (int) (16f * color.alphaF()), this.width * 7 / 8 + 16, this.height * 3 / 4 + 16 + (int) (16f * color.alphaF()))) {
             renderTooltip(stack, Tooltip.splitTooltip(Minecraft.getInstance(), Component.translatable("config.immersivemc.ranged_grab_color.desc")),
                     mouseX, mouseY);
         }
