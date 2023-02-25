a5b000fb44a190b85dce2dcc12d5616c5340dd6a
hammy3502 <hammy275@gmail.com>
2023-02-24 23:59:00 -0500

    Fabulous! Rendering Fix 1.19.3 Compat

--- common/src/main/java/com/hammy275/immersivemc/mixin/LevelRendererMixin.java
+++ common/src/main/java/com/hammy275/immersivemc/mixin/LevelRendererMixin.java
@@ -2,12 +2,12 @@ package com.hammy275.immersivemc.mixin;
 
 import com.hammy275.immersivemc.client.subscribe.ClientRenderSubscriber;
 import com.mojang.blaze3d.vertex.PoseStack;
-import com.mojang.math.Matrix4f;
 import net.minecraft.client.Camera;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.GameRenderer;
 import net.minecraft.client.renderer.LevelRenderer;
 import net.minecraft.client.renderer.LightTexture;
+import org.joml.Matrix4f;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
