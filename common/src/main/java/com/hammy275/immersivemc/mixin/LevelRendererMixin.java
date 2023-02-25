694d92eec7350e6a04c9abffdc3a922755dc4954
hammy3502 <hammy275@gmail.com>
2023-02-24 23:55:36 -0500

    Fix Immersives Not Rendering on Fabulous! Quality (closes #162)

---
+++ common/src/main/java/com/hammy275/immersivemc/mixin/LevelRendererMixin.java
@@ -0,0 +1,25 @@
+package com.hammy275.immersivemc.mixin;
+
+import com.hammy275.immersivemc.client.subscribe.ClientRenderSubscriber;
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.math.Matrix4f;
+import net.minecraft.client.Camera;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.GameRenderer;
+import net.minecraft.client.renderer.LevelRenderer;
+import net.minecraft.client.renderer.LightTexture;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(LevelRenderer.class)
+public class LevelRendererMixin {
+
+    @Inject(method="renderLevel", at=
+            @At(value = "INVOKE_STRING", args="ldc=particles", target="Lnet/minecraft/util/profiling/ProfilerFiller;popPush(Ljava/lang/String;)V"))
+    private void renderLevelWithParticles(PoseStack poseStack, float f, long l, boolean bl, Camera camera, GameRenderer gameRenderer, LightTexture lightTexture, Matrix4f matrix4f, CallbackInfo ci) {
+        Minecraft.getInstance().getProfiler().popPush("immersivemc");
+        ClientRenderSubscriber.onWorldRender(poseStack);
+    }
+}
