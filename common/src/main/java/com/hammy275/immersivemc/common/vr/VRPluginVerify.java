5cd0e97c2e41ed9e0992c6ca2ba79fbc8efb44f1
hammy275 <hammy275@gmail.com>
2023-05-07 15:15:43 -0400

    Fix Config Screen Crash when not in world (closes #250)

--- common/src/main/java/com/hammy275/immersivemc/common/vr/VRPluginVerify.java
+++ common/src/main/java/com/hammy275/immersivemc/common/vr/VRPluginVerify.java
@@ -3,6 +3,8 @@ package com.hammy275.immersivemc.common.vr;
 public class VRPluginVerify {
 
     public static boolean hasAPI = false;
+
+    // Only checks for API if not in-world
     public static boolean clientInVR() {
         return hasAPI && VRPluginVerifyProxy.vrAPIIInVR();
     }
