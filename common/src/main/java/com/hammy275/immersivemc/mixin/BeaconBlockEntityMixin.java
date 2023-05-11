bde1b05ccfedb7f818d08335acca545d25eec9ae
hammy3502 <hammy275@gmail.com>
2023-05-11 16:22:30 -0400

    Beacon Improvements and Fixes (closes #206)

--- common/src/main/java/com/hammy275/immersivemc/mixin/BeaconBlockEntityMixin.java
+++ common/src/main/java/com/hammy275/immersivemc/mixin/BeaconBlockEntityMixin.java
@@ -1,5 +1,6 @@
 package com.hammy275.immersivemc.mixin;
 
+import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.inventory.ContainerData;
 import net.minecraft.world.level.block.entity.BeaconBlockEntity;
 import org.spongepowered.asm.mixin.Mixin;
@@ -10,6 +11,12 @@ public interface BeaconBlockEntityMixin {
 
     @Accessor("levels")
     public int getLevels();
+
+    @Accessor("primaryPower")
+    public MobEffect getPrimaryPower();
+
+    @Accessor("secondaryPower")
+    public MobEffect getSecondaryPower();
 
     @Accessor("dataAccess")
     public ContainerData getBeaconData();
