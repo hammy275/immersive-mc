cc9599e31f5f0343a34560e4233d2f8ec6b31f79
hammy275 <hammy275@gmail.com>
2023-05-14 19:22:44 -0400

    Bag Tuning and Angle Checks (closes #265)

--- common/src/main/java/com/hammy275/immersivemc/client/immersive/ImmersiveHitboxes.java
+++ common/src/main/java/com/hammy275/immersivemc/client/immersive/ImmersiveHitboxes.java
@@ -8,6 +8,7 @@ import com.hammy275.immersivemc.common.vr.VRPlugin;
 import com.hammy275.immersivemc.common.vr.VRPluginVerify;
 import com.mojang.blaze3d.vertex.PoseStack;
 import dev.architectury.platform.Platform;
+import net.blf02.vrapi.api.data.IVRData;
 import net.minecraft.client.Minecraft;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.InteractionHand;
@@ -39,8 +40,7 @@ public class ImmersiveHitboxes extends AbstractImmersive<ImmersiveHitboxesInfo> 
             // centerPos is the center of the back of the player
             Vec3 centerPos = Minecraft.getInstance().player.position().add(0, 1.0625, 0);
             Vec3 headLook;
-            // Even though it's VR-Only, let's try to get something so we can send the failure bag open message
-            // and for desktop testing purposes
+            // Even though it's VR-Only, let's try to get something for desktop testing purposes
             if (VRPluginVerify.clientInVR() && VRPlugin.API.playerInVR(Minecraft.getInstance().player)
             && !Platform.isDevelopmentEnvironment()) {
                 headLook = VRPlugin.API.getVRPlayer(Minecraft.getInstance().player).getHMD().getLookAngle();
@@ -49,15 +49,24 @@ public class ImmersiveHitboxes extends AbstractImmersive<ImmersiveHitboxesInfo> 
             }
             headLook = headLook.multiply(1, 0, 1).normalize(); // Ignore y rotation
             centerPos = centerPos.add(headLook.scale(-0.25));
-            // Back is 0.5 blocks across from center, making size 0.5 for x and z.
+            // Back is 0.5 blocks across from center, making size 0.35 for x and z (full back has funny accidental detections).
             // We swap x and z since if we're looking along z, we want it to be big on the x axis and vice-versa
-            // Add 0.25 to have some sane minimum
+            // Add 0.2 to have some sane minimum
             info.setHitbox(ImmersiveHitboxesInfo.BACKPACK_INDEX,
                     AABB.ofSize(centerPos,
-                            Math.max(Math.abs(headLook.z) * 0.5, 1d/4d),
+                            Math.max(Math.abs(headLook.z) * 0.35, 0.2),
                             backpackHeight,
-                            Math.max(Math.abs(headLook.x) * 0.5, 1d/4d)
+                            Math.max(Math.abs(headLook.x) * 0.35, 0.2)
                     ));
+            if (VRPluginVerify.clientInVR() && VRPlugin.API.playerInVR(Minecraft.getInstance().player)) {
+                IVRData secondaryController = VRPlugin.API.getVRPlayer(Minecraft.getInstance().player).getController1();
+                IVRData hmd = VRPlugin.API.getVRPlayer(Minecraft.getInstance().player).getHMD();
+                if (info.getHitbox(ImmersiveHitboxesInfo.BACKPACK_INDEX)
+                        .contains(secondaryController.position())) {
+                    info.validBackpackAngle = controllerOpenBagAngleCheck(hmd,
+                            secondaryController);
+                }
+            }
         } else {
             // In case setting changes mid-game
             info.setHitbox(ImmersiveHitboxesInfo.BACKPACK_INDEX, null);
@@ -77,6 +86,15 @@ public class ImmersiveHitboxes extends AbstractImmersive<ImmersiveHitboxesInfo> 
         AABB backpackHitbox = info.getHitbox(ImmersiveHitboxesInfo.BACKPACK_INDEX);
         if (backpackHitbox != null) {
             renderHitbox(stack, backpackHitbox, backpackHitbox.getCenter());
+            if (VRPluginVerify.hasAPI && VRPlugin.API.playerInVR(Minecraft.getInstance().player)
+            && Minecraft.getInstance().getEntityRenderDispatcher().shouldRenderHitBoxes()) {
+                IVRData c1 = VRPlugin.API.getVRPlayer(Minecraft.getInstance().player).getController1();
+                if (backpackHitbox.contains(c1.position())) {
+                    renderHitbox(stack, AABB.ofSize(c1.position(), 0.25, 0.25, 0.25),
+                            c1.position(), true,
+                            info.validBackpackAngle ? 0f : 1f, info.validBackpackAngle ? 1f : 0f, 0f);
+                }
+            }
         }
     }
 
@@ -117,11 +135,14 @@ public class ImmersiveHitboxes extends AbstractImmersive<ImmersiveHitboxesInfo> 
 
     @Override
     public void handleRightClick(AbstractImmersiveInfo info, Player player, int closest, InteractionHand hand) {
-        if (closest == ImmersiveHitboxesInfo.BACKPACK_INDEX && hand == InteractionHand.OFF_HAND
-        && backpackCooldown <= 0) {
-            ClientUtil.openBag(player);
-            backpackCooldown = 50;
+        if (info instanceof ImmersiveHitboxesInfo hInfo) {
+            if (closest == ImmersiveHitboxesInfo.BACKPACK_INDEX && hand == InteractionHand.OFF_HAND
+                    && backpackCooldown <= 0 && hInfo.validBackpackAngle) {
+                ClientUtil.openBag(player);
+                backpackCooldown = 50;
+            }
         }
+
     }
 
     @Override
@@ -133,5 +154,18 @@ public class ImmersiveHitboxes extends AbstractImmersive<ImmersiveHitboxesInfo> 
         if (this.infos.size() == 0) {
             this.infos.add(new ImmersiveHitboxesInfo());
         }
+    }
+
+    private boolean controllerOpenBagAngleCheck(IVRData hmd, IVRData controller) {
+        // Ignore y axis here (like with the bag boxes in the first place!)
+        Vec3 handToHeadVec = controller.position().vectorTo(Minecraft.getInstance().player.getEyePosition())
+                .multiply(1, 0, 1).normalize();
+        Vec3 headVec = hmd.getLookAngle().multiply(1, 0, 1).normalize();
+
+        // Get angle from 0 to PI, where PI is completely opposite the vector and 0 is same direction as vector
+        double angle = Math.acos(headVec.dot(handToHeadVec));
+
+        // 60 degree margin, but they should be the same direction
+        return angle <= Math.PI / 3;
     }
 }
