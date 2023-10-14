72ef171a2a4e3da2a8a5e17d8d967a20c030f8ae
hammy275 <hammy275@gmail.com>
2023-10-14 14:14:50 -0400

    Immersive Anvil and ImmersiveBuilder Additions For It

--- common/src/main/java/com/hammy275/immersivemc/client/immersive/Immersives.java
+++ common/src/main/java/com/hammy275/immersivemc/client/immersive/Immersives.java
@@ -2,12 +2,16 @@ package com.hammy275.immersivemc.client.immersive;
 
 import com.hammy275.immersivemc.client.config.ClientConstants;
 import com.hammy275.immersivemc.client.immersive.info.AbstractImmersiveInfo;
+import com.hammy275.immersivemc.client.immersive.info.AnvilData;
 import com.hammy275.immersivemc.common.config.ActiveConfig;
 import com.hammy275.immersivemc.common.immersive.ImmersiveCheckers;
 import com.hammy275.immersivemc.common.network.Network;
 import com.hammy275.immersivemc.common.network.packet.InteractPacket;
 import com.hammy275.immersivemc.common.network.packet.SwapPacket;
+import com.hammy275.immersivemc.common.storage.AnvilStorage;
+import net.minecraft.client.resources.language.I18n;
 import net.minecraft.core.Direction;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.phys.Vec3;
 
 import java.util.LinkedList;
@@ -21,7 +25,34 @@ public class Immersives {
     public static final List<AbstractImmersive<? extends AbstractImmersiveInfo>> WS_IMMERSIVES =
             new LinkedList<>();
 
-    public static final ImmersiveAnvil immersiveAnvil = new ImmersiveAnvil();
+    public static final BuiltImmersive immersiveAnvil = ImmersiveBuilder.create(ImmersiveCheckers::isAnvil)
+            .setConfigChecker(() -> ActiveConfig.useAnvilImmersion)
+            .setRenderTime(ClientConstants.ticksToRenderAnvil)
+            .setRenderSize(ClientConstants.itemScaleSizeAnvil)
+            .addHitbox(HitboxInfoBuilder.createItemInput(new Vec3(0, -1d/3d, 0), // When you place an anvil, the anvil's look direction is rotated 90 degrees.
+                    ClientConstants.itemScaleSizeAnvil).upDownRenderDir(Direction.UP).build())
+            .addHitbox(HitboxInfoBuilder.createItemInput(new Vec3(0d, 0, 0),
+                    ClientConstants.itemScaleSizeAnvil).upDownRenderDir(Direction.UP).build())
+            .addHitbox(HitboxInfoBuilder.create(new Vec3(0, 1d/3d, 0),
+                    ClientConstants.itemScaleSizeAnvil).holdsItems(true).upDownRenderDir(Direction.UP).build())
+            .addHitbox(HitboxInfoBuilder.create(new Vec3(0, 0, 0.5), 0)
+                    .textSupplier((info) -> {
+                        AnvilData data = (AnvilData) info.getExtraData();
+                        if (data.anvilCost == 0) return null;
+                        return Component.literal(I18n.get("immersivemc.immersive.anvil.levels_needed", data.anvilCost));
+                    })
+                    .build())
+            .setExtraStorageConsumer((storage, info) -> {
+                AnvilStorage aStorage = (AnvilStorage) storage;
+                AnvilData data = (AnvilData) info.getExtraData();
+                data.anvilCost = aStorage.xpLevels;
+            })
+            .setPositioningMode(HitboxPositioningMode.TOP_BLOCK_FACING)
+            .setMaxImmersives(1)
+            .setRightClickHandler((info, player, slot, hand) -> Network.INSTANCE.sendToServer(new InteractPacket(info.getBlockPosition(), slot, hand)))
+            .setUsesWorldStorage(true)
+            .setExtraInfoDataClass(AnvilData.class)
+            .build();
     public static final ImmersiveBackpack immersiveBackpack = new ImmersiveBackpack();
     public static final ImmersiveBarrel immersiveBarrel = new ImmersiveBarrel();
     public static final ImmersiveBeacon immersiveBeacon = new ImmersiveBeacon();
