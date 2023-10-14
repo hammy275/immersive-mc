97278ba889083512ba6054428910590e781fa27b
hammy275 <hammy275@gmail.com>
2023-10-13 23:14:54 -0400

    Port Immersive Jukebox

--- common/src/main/java/com/hammy275/immersivemc/client/immersive/HitboxPositioningMode.java
+++ common/src/main/java/com/hammy275/immersivemc/client/immersive/HitboxPositioningMode.java
@@ -1,6 +1,7 @@
 package com.hammy275.immersivemc.client.immersive;
 
 public enum HitboxPositioningMode {
-    HORIZONTAL_BLOCK_FACING,
-    PLAYER_FACING
+    HORIZONTAL_BLOCK_FACING, // Face that block is facing, excluding up and down face
+    PLAYER_FACING, // Top of block face, oriented on player look direction
+    TOP_LITERAL // Top of block face, literal orientation (x is always in-game x, etc.)
 }
