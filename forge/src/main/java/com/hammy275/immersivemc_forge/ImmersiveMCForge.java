84f1570403334c6499df2c46249e2fca371c5487
hammy275 <hammy275@gmail.com>
2023-10-13 20:30:39 -0400

    Forgot a Forge Change For the 1.20.2 Port

--- forge/src/main/java/com/hammy275/immersivemc_forge/ImmersiveMCForge.java
+++ forge/src/main/java/com/hammy275/immersivemc_forge/ImmersiveMCForge.java
@@ -10,14 +10,13 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.fml.loading.FMLEnvironment;
-import net.minecraftforge.network.NetworkConstants;
 
 @Mod(ImmersiveMC.MOD_ID)
 public class ImmersiveMCForge {
     public ImmersiveMCForge() {
         EventBuses.registerModEventBus(ImmersiveMC.MOD_ID, FMLJavaModLoadingContext.get().getModEventBus());
         ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class,
-                () -> new IExtensionPoint.DisplayTest(() -> NetworkConstants.IGNORESERVERONLY, (a, b) -> true));
+                () -> new IExtensionPoint.DisplayTest(() -> IExtensionPoint.DisplayTest.IGNORESERVERONLY, (a, b) -> true));
         ImmersiveMC.init();
         if (FMLEnvironment.dist == Dist.CLIENT) {
             ClientSetup.doClientSetup();
