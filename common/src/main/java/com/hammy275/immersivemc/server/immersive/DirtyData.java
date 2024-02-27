86bfc7fa0c8eeee710985567da2ae4cd2e81cefe
hammy275 <hammy275@gmail.com>
2024-02-27 16:00:49 -0500

    Remove Unused DirtyData

--- common/src/main/java/com/hammy275/immersivemc/server/immersive/DirtyData.java
+++
@@ -1,7 +0,0 @@
-package com.hammy275.immersivemc.server.immersive;
-
-import net.minecraft.core.BlockPos;
-import net.minecraft.world.level.Level;
-
-public record DirtyData(BlockPos pos, Level level) {
-}
