6f218ae7961ad40c5814c478b8736be3347b487c
hammy275 <hammy275@gmail.com>
2024-03-03 14:33:04 -0500

    Lootr Fixes and Bugfixes

--- common/src/main/java/com/hammy275/immersivemc/common/compat/lootr/LootrCompat.java
+++ common/src/main/java/com/hammy275/immersivemc/common/compat/lootr/LootrCompat.java
@@ -4,6 +4,7 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.Container;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
 import org.jetbrains.annotations.Nullable;
 
 public interface LootrCompat {
@@ -24,4 +25,37 @@ public interface LootrCompat {
      * @param pos    Position of chest
      */
     public void markOpener(Player player, BlockPos pos);
+
+    /**
+     * @param pos   Position to check
+     * @param level Level to check in
+     * @return Whether the block at the given position is a Lootr container.
+     */
+    public boolean isLootrContainer(BlockPos pos, Level level);
+
+    /**
+     * Open/close Lootr barrel if it is one.
+     * @param pos The position of the potential barrel.
+     * @param player Player that is opening/closing the barrel.
+     * @param nowOpen Whether the player is opening or closing the barrel.
+     * @return Whether the open/close succeeded. Fails when it isn't a Lootr barrel.
+     */
+    public boolean openLootrBarrel(BlockPos pos, Player player, boolean nowOpen);
+
+    /**
+     * Open/close Lootr Shulker Box if it is one.
+     * @param pos The position of the potential Shulker Box.
+     * @param player Player that is opening/closing the Shulker Box.
+     * @param nowOpen Whether the player is opening or closing the Shulker Box.
+     * @return Whether the open/close succeeded. Fails when it isn't a Lootr Shulker Box.
+     */
+    public boolean openLootrShulkerBox(BlockPos pos, Player player, boolean nowOpen);
+
+    /**
+     * @param pos Position to check.
+     * @param player Player to check for.
+     * @return Whether the supplied Lootr block is open.
+     */
+    public boolean isOpen(BlockPos pos, Player player);
+
 }
--- forge/src/main/java/com/hammy275/immersivemc_forge/LootrCompatImpl.java
+++ forge/src/main/java/com/hammy275/immersivemc_forge/LootrCompatImpl.java
@@ -1,17 +1,22 @@
 package com.hammy275.immersivemc_forge;
 
 import com.hammy275.immersivemc.common.compat.lootr.LootrCompat;
+import com.hammy275.immersivemc.common.network.packet.ChestShulkerOpenPacket;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.Container;
 import net.minecraft.world.MenuProvider;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.BarrelBlock;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.RandomizableContainerBlockEntity;
 import noobanidus.mods.lootr.api.LootrAPI;
 import noobanidus.mods.lootr.api.blockentity.ILootBlockEntity;
 import noobanidus.mods.lootr.api.inventory.ILootrInventory;
+import noobanidus.mods.lootr.block.entities.LootrBarrelBlockEntity;
+import noobanidus.mods.lootr.block.entities.LootrChestBlockEntity;
+import noobanidus.mods.lootr.block.entities.LootrShulkerBlockEntity;
 import org.jetbrains.annotations.Nullable;
 
 public class LootrCompatImpl implements LootrCompat {
@@ -43,4 +48,53 @@ public class LootrCompatImpl implements LootrCompat {
             }
         }
     }
+
+    @Override
+    public boolean isLootrContainer(BlockPos pos, Level level) {
+        return level.getBlockEntity(pos) instanceof ILootBlockEntity;
+    }
+
+    @Override
+    public boolean openLootrBarrel(BlockPos pos, Player player, boolean nowOpen) {
+        if (player.level().getBlockEntity(pos) instanceof LootrBarrelBlockEntity lbbe) {
+            if (nowOpen) {
+                lbbe.startOpen(player);
+                ChestShulkerOpenPacket.changeChestCount(pos, 1);
+            } else {
+                lbbe.stopOpen(player);
+                ChestShulkerOpenPacket.changeChestCount(pos, -1);
+            }
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean openLootrShulkerBox(BlockPos pos, Player player, boolean nowOpen) {
+        if (player.level().getBlockEntity(pos) instanceof LootrShulkerBlockEntity lsbe) {
+            if (nowOpen) {
+                lsbe.startOpen(player);
+                ChestShulkerOpenPacket.changeChestCount(pos, 1);
+            } else {
+                lsbe.stopOpen(player);
+                ChestShulkerOpenPacket.changeChestCount(pos, -1);
+            }
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean isOpen(BlockPos pos, Player player) {
+        BlockEntity be = player.level().getBlockEntity(pos);
+        if (be instanceof LootrShulkerBlockEntity lsbe) {
+            return !lsbe.isClosed();
+        } else if (be instanceof LootrBarrelBlockEntity) {
+            return player.level().getBlockState(pos).getValue(BarrelBlock.OPEN);
+        } else if (be instanceof LootrChestBlockEntity lcbe) {
+            return lcbe.getOpenNess(1f) > 0;
+        }
+        return false;
+    }
+
 }
