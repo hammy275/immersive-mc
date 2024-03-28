2f386e24202155e341fcb132d1b5677925583056
hammy275 <hammy275@gmail.com>
2024-03-27 21:33:04 -0400

    Update to Architectury Loom 1.5

--- common/src/main/java/com/hammy275/immersivemc/common/vr/mixin_proxy/LivingEntityMixinProxy.java
+++ common/src/main/java/com/hammy275/immersivemc/common/vr/mixin_proxy/LivingEntityMixinProxy.java
@@ -12,8 +12,7 @@ import net.minecraft.world.entity.projectile.AbstractArrow;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.UseAnim;
 import net.minecraft.world.phys.Vec3;
-
-import javax.annotation.Nullable;
+import org.jetbrains.annotations.Nullable;
 
 public class LivingEntityMixinProxy {
 
