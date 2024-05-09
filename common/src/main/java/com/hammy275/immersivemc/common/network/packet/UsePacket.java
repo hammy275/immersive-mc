bf1a5a83f98c225b6683c30580283cced3ccde55
hammy275 <hammy275@gmail.com>
2024-05-08 23:41:41 -0400

    Move Lever to Hitbox System (closes #224)

---
+++ common/src/main/java/com/hammy275/immersivemc/common/network/packet/UsePacket.java
@@ -0,0 +1,36 @@
+package com.hammy275.immersivemc.common.network.packet;
+
+import com.hammy275.immersivemc.common.immersive.ImmersiveCheckers;
+import com.hammy275.immersivemc.common.util.Util;
+import dev.architectury.networking.NetworkManager;
+import net.minecraft.core.BlockPos;
+import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.server.level.ServerPlayer;
+
+import java.util.function.Supplier;
+
+public class UsePacket {
+
+    public final BlockPos pos;
+
+    public UsePacket(BlockPos pos) {
+        this.pos = pos;
+    }
+
+    public static void encode(UsePacket message, FriendlyByteBuf buffer) {
+        buffer.writeBlockPos(message.pos);
+    }
+
+    public static UsePacket decode(FriendlyByteBuf buffer) {
+        return new UsePacket(buffer.readBlockPos());
+    }
+
+    public static void handle(final UsePacket packet, Supplier<NetworkManager.PacketContext> ctx) {
+        ctx.get().queue(() -> {
+            ServerPlayer player = ctx.get().getPlayer() instanceof ServerPlayer ? (ServerPlayer) ctx.get().getPlayer() : null;
+            if (player != null && ImmersiveCheckers.isLever(packet.pos, player.level)) {
+                Util.useLever(player, packet.pos);
+            }
+        });
+    }
+}
