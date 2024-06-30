223488959f6aac84a9dc0885d78afe4b1ac0f913
hammy275 <hammy275@gmail.com>
2024-06-30 13:53:14 -0400

    Block Joins if Server<-->Client Immersives Mismatch

--- common/src/main/java/com/hammy275/immersivemc/common/network/NetworkClientHandlers.java
+++ common/src/main/java/com/hammy275/immersivemc/common/network/NetworkClientHandlers.java
@@ -1,26 +1,56 @@
 package com.hammy275.immersivemc.common.network;
 
+import com.hammy275.immersivemc.ImmersiveMC;
 import com.hammy275.immersivemc.api.client.immersive.Immersive;
 import com.hammy275.immersivemc.api.client.immersive.ImmersiveInfo;
 import com.hammy275.immersivemc.api.common.immersive.ImmersiveHandler;
+import com.hammy275.immersivemc.api.common.immersive.NetworkStorage;
 import com.hammy275.immersivemc.client.immersive.AbstractPlayerAttachmentImmersive;
 import com.hammy275.immersivemc.client.immersive.Immersives;
 import com.hammy275.immersivemc.client.immersive.info.AbstractImmersiveInfo;
 import com.hammy275.immersivemc.client.immersive.info.BackpackInfo;
 import com.hammy275.immersivemc.client.immersive.info.BeaconInfo;
 import com.hammy275.immersivemc.client.subscribe.ClientLogicSubscriber;
-import com.hammy275.immersivemc.api.common.immersive.NetworkStorage;
+import com.hammy275.immersivemc.common.immersive.handler.ImmersiveHandlers;
 import com.hammy275.immersivemc.common.network.packet.BeaconDataPacket;
 import com.hammy275.immersivemc.common.util.Util;
 import com.hammy275.immersivemc.common.vr.VRRumble;
 import net.minecraft.client.Minecraft;
 import net.minecraft.core.BlockPos;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 
-import java.util.Objects;
+import java.util.*;
 
 public class NetworkClientHandlers {
+
+    public static void checkHandlerMatch(List<ResourceLocation> serverHandlerIDs) {
+        Map<ResourceLocation, ImmersiveHandler<?>> clientHandlers = new HashMap<>();
+        ImmersiveHandlers.HANDLERS.forEach((handler) -> clientHandlers.put(handler.getID(), handler));
+
+        List<ResourceLocation> serverOnly = serverHandlerIDs.stream().filter((id) -> !clientHandlers.containsKey(id)).toList();
+        List<ResourceLocation> clientOnly = clientHandlers.entrySet().stream().filter((entry) ->
+                !serverHandlerIDs.contains(entry.getKey()) && !entry.getValue().clientAuthoritative())
+                .map(Map.Entry::getKey).toList();
+        if (!serverOnly.isEmpty() || !clientOnly.isEmpty()) {
+            if (!serverOnly.isEmpty()) {
+                ImmersiveMC.LOGGER.error("The following Immersives were on the server, but not in your game:");
+                ImmersiveMC.LOGGER.error(String.join(", ", serverOnly.stream().map(ResourceLocation::toString).toList()));
+            }
+            if (!clientOnly.isEmpty()) {
+                ImmersiveMC.LOGGER.error("The following Immersives are in your game, but not on the server even though they're required to be:");
+                ImmersiveMC.LOGGER.error(String.join(", ", clientOnly.stream().map(ResourceLocation::toString).toList()));
+            }
+            Set<String> missingModIDs = new HashSet<>();
+            serverOnly.forEach((id) -> missingModIDs.add(id.getNamespace()));
+            clientOnly.forEach((id) -> missingModIDs.add(id.getNamespace()));
+
+            Minecraft.getInstance().getConnection().onDisconnect(Component.translatable("message.immersivemc.missing_immersives",
+                    String.join(", ", missingModIDs)));
+        }
+    }
 
     public static void setBeaconData(BeaconDataPacket packet) {
         for (BeaconInfo info : Immersives.immersiveBeacon.getTrackedObjects()) {
--- common/src/main/java/com/hammy275/immersivemc/common/network/packet/ConfigSyncPacket.java
+++ common/src/main/java/com/hammy275/immersivemc/common/network/packet/ConfigSyncPacket.java
@@ -2,64 +2,78 @@ package com.hammy275.immersivemc.common.network.packet;
 
 import com.hammy275.immersivemc.common.config.ActiveConfig;
 import com.hammy275.immersivemc.common.network.Network;
+import com.hammy275.immersivemc.common.network.NetworkClientHandlers;
 import com.hammy275.immersivemc.server.immersive.TrackedImmersives;
 import dev.architectury.networking.NetworkManager;
 import net.minecraft.network.FriendlyByteBuf;
-import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerPlayer;
+import org.jetbrains.annotations.Nullable;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.function.Supplier;
 
+/**
+ * Syncs config data and information about the ImmersiveMC state between the server and client.
+ * Starts with an S2C packet, then a C2S packet afterward during the initial handshake.
+ * <br>
+ * Server and client can update their respective config states as well at anytime by sending this packet without
+ * the handlerIDs.
+ */
 public class ConfigSyncPacket {
 
-    public boolean kickMe;
-    public ActiveConfig config;
-
-    public ConfigSyncPacket(boolean isKickMe) {
-        kickMe = isKickMe;
-        config = null;
-    }
+    public final ActiveConfig config;
+    @Nullable
+    public final List<ResourceLocation> handlerIDs;
 
     public ConfigSyncPacket(ActiveConfig config) {
-        kickMe = false;
+        this(config, null);
+    }
+
+    public ConfigSyncPacket(ActiveConfig config, @Nullable List<ResourceLocation> handlerIDs) {
         this.config = config;
+        this.handlerIDs = handlerIDs;
     }
 
     public static void encode(ConfigSyncPacket packet, FriendlyByteBuf buffer) {
-        buffer.writeBoolean(packet.kickMe); // Write if we're asking the server to kick us
-        if (!packet.kickMe) { // If we don't want to kick us, write the config data
-            packet.config.encode(buffer);
+        packet.config.encode(buffer);
+        buffer.writeInt(packet.handlerIDs == null ? 0 : packet.handlerIDs.size());
+        if (packet.handlerIDs != null) {
+            packet.handlerIDs.forEach(buffer::writeResourceLocation);
         }
     }
 
     public static ConfigSyncPacket decode(FriendlyByteBuf buffer) {
-        if (buffer.readBoolean()) { // If we want to be kicked, just decode a kick packet
-            return new ConfigSyncPacket(true);
-        } else {
-            ActiveConfig incoming = new ActiveConfig();
-            incoming.decode(buffer);
-            return new ConfigSyncPacket(incoming);
+        ActiveConfig incoming = new ActiveConfig();
+        incoming.decode(buffer);
+        List<ResourceLocation> handlerIDs = null;
+        int numIDs = buffer.readInt();
+        if (numIDs > 0) {
+            handlerIDs = new ArrayList<>();
+            for (int i = 0; i < numIDs; i++) {
+                handlerIDs.add(buffer.readResourceLocation());
+            }
         }
+        return new ConfigSyncPacket(incoming, handlerIDs);
     }
 
     public static void handle(final ConfigSyncPacket message, Supplier<NetworkManager.PacketContext> ctx) {
         ctx.get().queue(() -> {
             ServerPlayer player = ctx.get().getPlayer() instanceof ServerPlayer ? (ServerPlayer) ctx.get().getPlayer() : null;
             if (player == null) { // S2C
+                // Check handlers and kick on mismatch
+                NetworkClientHandlers.checkHandlerMatch(message.handlerIDs);
                 // Load server config
                 ActiveConfig.FROM_SERVER = message.config;
                 ActiveConfig.loadActive();
                 // Send server our config
                 Network.INSTANCE.sendToServer(new ConfigSyncPacket(ActiveConfig.FILE));
-            } else if (message.kickMe) { // C2S asking to be kicked
-                ((ServerPlayer) ctx.get().getPlayer()).connection.disconnect(
-                        Component.literal("The server is using a different version of ImmersiveMC than you!"));
             } else { // C2S sending us a config
                 message.config.mergeWithOther(ActiveConfig.FILE);
                 ActiveConfig.registerPlayerConfig(ctx.get().getPlayer(), message.config);
                 TrackedImmersives.clearForPlayer(player);
             }
         });
-        
     }
 }
