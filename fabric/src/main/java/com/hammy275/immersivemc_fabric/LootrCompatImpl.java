6143da36d65b3f21e525a2e074c96704851a8a9a
hammy275 <hammy275@gmail.com>
2024-09-06 18:21:25 -0400

    Address Lootr Package Name Change and Dirtiness Check (#413 and #439)

--- fabric/src/main/java/com/hammy275/immersivemc_fabric/LootrCompatImpl.java
+++ fabric/src/main/java/com/hammy275/immersivemc_fabric/LootrCompatImpl.java
@@ -4,6 +4,7 @@ import com.hammy275.immersivemc.common.compat.Lootr;
 import com.hammy275.immersivemc.common.compat.lootr.LootrCompat;
 import com.hammy275.immersivemc.common.compat.lootr.LootrNullImpl;
 import com.hammy275.immersivemc.common.compat.util.CompatModule;
+import com.hammy275.immersivemc.mixin.ChestLidControllerAccessor;
 import com.hammy275.immersivemc.server.ChestToOpenSet;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerPlayer;
@@ -17,12 +18,37 @@ import net.minecraft.world.level.block.entity.RandomizableContainerBlockEntity;
 import net.zestyblaze.lootr.api.LootrAPI;
 import net.zestyblaze.lootr.api.blockentity.ILootBlockEntity;
 import net.zestyblaze.lootr.api.inventory.ILootrInventory;
-import net.zestyblaze.lootr.blocks.entities.LootrBarrelBlockEntity;
-import net.zestyblaze.lootr.blocks.entities.LootrChestBlockEntity;
-import net.zestyblaze.lootr.blocks.entities.LootrShulkerBlockEntity;
 import org.jetbrains.annotations.Nullable;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 public class LootrCompatImpl implements LootrCompat {
+
+    private static final Class<?> lootrBarrelBlockEntityClass;
+    private static final Method barrelStartOpen;
+    private static final Method barrelStopOpen;
+    private static final Class<?> lootrChestBlockEntityClass;
+    private static final Field chestChestLidController;
+    private static final Class<?> lootrShulkerBlockEntityClass;
+    private static final Method shulkerBoxStartOpen;
+    private static final Method shulkerBoxStopOpen;
+    private static final Method shulkerBoxIsClosed;
+
+    static {
+        lootrBarrelBlockEntityClass = getLootrClassMaybeRenamed("net.zestyblaze.lootr.blocks.entities.LootrBarrelBlockEntity");
+        barrelStartOpen = getMethod(lootrBarrelBlockEntityClass, "startOpen", Player.class);
+        barrelStopOpen = getMethod(lootrBarrelBlockEntityClass, "stopOpen", Player.class);
+
+        lootrChestBlockEntityClass = getLootrClassMaybeRenamed("net.zestyblaze.lootr.blocks.entities.LootrChestBlockEntity");
+        chestChestLidController = getDeclaredField(lootrChestBlockEntityClass, "chestLidController");
+
+        lootrShulkerBlockEntityClass = getLootrClassMaybeRenamed("net.zestyblaze.lootr.blocks.entities.LootrShulkerBlockEntity");
+        shulkerBoxStartOpen = getMethod(lootrShulkerBlockEntityClass, "startOpen", Player.class);
+        shulkerBoxStopOpen = getMethod(lootrShulkerBlockEntityClass, "stopOpen", Player.class);
+        shulkerBoxIsClosed = getMethod(lootrShulkerBlockEntityClass, "isClosed");
+    }
 
     private LootrCompatImpl() {}
 
@@ -69,12 +95,13 @@ public class LootrCompatImpl implements LootrCompat {
 
     @Override
     public boolean openLootrBarrel(BlockPos pos, Player player, boolean nowOpen) {
-        if (player.level.getBlockEntity(pos) instanceof LootrBarrelBlockEntity lbbe) {
+        BlockEntity barrel = player.level.getBlockEntity(pos);
+        if (lootrBarrelBlockEntityClass.isInstance(barrel)) {
             if (nowOpen) {
-                lbbe.startOpen(player);
+                invoke(barrelStartOpen, barrel, player);
                 ChestToOpenSet.openChest(player, pos);
             } else {
-                lbbe.stopOpen(player);
+                invoke(barrelStopOpen, barrel, player);
                 ChestToOpenSet.closeChest(player, pos);
             }
             return true;
@@ -84,12 +111,13 @@ public class LootrCompatImpl implements LootrCompat {
 
     @Override
     public boolean openLootrShulkerBox(BlockPos pos, Player player, boolean nowOpen) {
-        if (player.level.getBlockEntity(pos) instanceof LootrShulkerBlockEntity lsbe) {
+        BlockEntity be = player.level.getBlockEntity(pos);
+        if (lootrShulkerBlockEntityClass.isInstance(be)) {
             if (nowOpen) {
-                lsbe.startOpen(player);
+                invoke(shulkerBoxStartOpen, be, player);
                 ChestToOpenSet.openChest(player, pos);
             } else {
-                lsbe.stopOpen(player);
+                invoke(shulkerBoxStopOpen, be, player);
                 ChestToOpenSet.closeChest(player, pos);
             }
             return true;
@@ -100,14 +128,65 @@ public class LootrCompatImpl implements LootrCompat {
     @Override
     public boolean isOpen(BlockPos pos, Player player) {
         BlockEntity be = player.level.getBlockEntity(pos);
-        if (be instanceof LootrShulkerBlockEntity lsbe) {
-            return !lsbe.isClosed();
-        } else if (be instanceof LootrBarrelBlockEntity) {
+        if (lootrShulkerBlockEntityClass.isInstance(be)) {
+            return !((boolean) invoke(shulkerBoxIsClosed, be));
+        } else if (lootrBarrelBlockEntityClass.isInstance(be)) {
             return player.level.getBlockState(pos).getValue(BarrelBlock.OPEN);
-        } else if (be instanceof LootrChestBlockEntity lcbe) {
-            return lcbe.getOpenNess(1f) > 0;
+        } else if (lootrChestBlockEntityClass.isInstance(be)) {
+            try {
+                return ((ChestLidControllerAccessor) chestChestLidController.get(be)).getShouldBeOpen();
+            } catch (IllegalAccessException ignored) {}
         }
         return false;
     }
 
+    @Nullable
+    private static Class<?> getLootrClassMaybeRenamed(String name) {
+        try {
+            return Class.forName(name);
+        } catch (ClassNotFoundException ignored) {
+            try {
+                return Class.forName(name.replace(".blocks.", ".block."));
+            } catch (ClassNotFoundException ignored2) {
+                return null;
+            }
+        }
+    }
+
+    @Nullable
+    private static Method getMethod(@Nullable Class<?> clazz, String methodName, Class<?>... parameterTypes) {
+        if (clazz == null) {
+            return null;
+        }
+        try {
+            return clazz.getMethod(methodName, parameterTypes);
+        } catch (NoSuchMethodException ignored) {
+            return null;
+        }
+    }
+
+    @Nullable
+    private static Field getDeclaredField(@Nullable Class<?> clazz, String fieldName) {
+        if (clazz == null) {
+            return null;
+        }
+        try {
+            Field field = clazz.getDeclaredField(fieldName);
+            field.setAccessible(true);
+            return field;
+        } catch (NoSuchFieldException ignored) {
+            return null;
+        }
+    }
+
+    private static Object invoke(Method method, Object obj, Object... args) {
+        if (method == null) {
+            return null;
+        }
+        try {
+            return method.invoke(obj, args);
+        } catch (InvocationTargetException | IllegalAccessException ignored) {
+            return null;
+        }
+    }
 }
