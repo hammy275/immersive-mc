b60d6a55ddbda462f4c9a7d84a2862c456204b4f
hammy275 <hammy275@gmail.com>
2024-09-08 21:56:21 -0400

    Drag Immersive System and Immersive Trapdoor (#172)

--- common/src/main/java/com/hammy275/immersivemc/common/network/packet/UsePacket.java
+++ common/src/main/java/com/hammy275/immersivemc/common/network/packet/UsePacket.java
@@ -1,11 +1,13 @@
 package com.hammy275.immersivemc.common.network.packet;
 
 import com.hammy275.immersivemc.common.immersive.ImmersiveCheckers;
+import com.hammy275.immersivemc.common.immersive.handler.ImmersiveHandlers;
 import com.hammy275.immersivemc.common.util.Util;
 import dev.architectury.networking.NetworkManager;
 import net.minecraft.core.BlockPos;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.level.Level;
 
 import java.util.function.Supplier;
 
@@ -28,8 +30,13 @@ public class UsePacket {
     public static void handle(final UsePacket packet, Supplier<NetworkManager.PacketContext> ctx) {
         ctx.get().queue(() -> {
             ServerPlayer player = ctx.get().getPlayer() instanceof ServerPlayer ? (ServerPlayer) ctx.get().getPlayer() : null;
-            if (player != null && ImmersiveCheckers.isLever(packet.pos, player.level())) {
-                Util.useLever(player, packet.pos);
+            if (player != null) {
+                Level level = player.level();
+                if (ImmersiveCheckers.isLever(packet.pos, level)) {
+                    Util.useLever(player, packet.pos);
+                } else if (ImmersiveHandlers.trapdoorHandler.isValidBlock(packet.pos, level)) {
+                    Util.useTrapdoor(player, level, packet.pos);
+                }
             }
         });
     }
