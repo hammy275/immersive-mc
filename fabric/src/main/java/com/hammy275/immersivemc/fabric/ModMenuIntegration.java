7a80ed5f60a990cb6d3364998af32d868dccd4bb
hammy275 <hammy275@gmail.com>
2024-09-13 12:05:11 -0400

    Rename for @ExpectPlatform and Remove Quilt Subproject

--- fabric/src/main/java/com/hammy275/immersivemc/fabric/ModMenuIntegration.java
+++ fabric/src/main/java/com/hammy275/immersivemc/fabric/ModMenuIntegration.java
@@ -1,4 +1,4 @@
-package com.hammy275.immersivemc_quilt;
+package com.hammy275.immersivemc.fabric;
 
 import com.hammy275.immersivemc.client.config.screen.ConfigScreen;
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
