93c117ef10ad7af4970be6adf450229e284f7975
hammy275 <hammy275@gmail.com>
2024-10-02 10:44:29 -0400

    Fix Disabling Vanilla Interactions in Config Screen (#477)

--- common/src/main/java/com/hammy275/immersivemc/client/config/screen/ImmersivesCustomizeScreen.java
+++ common/src/main/java/com/hammy275/immersivemc/client/config/screen/ImmersivesCustomizeScreen.java
@@ -20,7 +20,7 @@ public class ImmersivesCustomizeScreen extends OptionsSubScreen {
 
     @Override
     protected void addOptions() {
-        ScreenUtils.addOptionIfClient("disable_vanilla_guis", config -> config.disableVanillaInteractionsForSupportedImmersives, (config, newVal) -> config.disableImmersiveMCOutsideVR = newVal, this.list);
+        ScreenUtils.addOptionIfClient("disable_vanilla_guis", config -> config.disableVanillaInteractionsForSupportedImmersives, (config, newVal) -> config.disableVanillaInteractionsForSupportedImmersives = newVal, this.list);
         ScreenUtils.addOptionIfClient("return_items", config -> config.returnItemsWhenLeavingImmersives, (config, newVal) -> config.returnItemsWhenLeavingImmersives = newVal, this.list);
         ScreenUtils.addOptionIfClient("do_rumble", config -> config.doVRControllerRumble, (config, newVal) -> config.doVRControllerRumble = newVal, this.list);
         ScreenUtils.addOptionIfClient("center_brewing", config -> config.autoCenterBrewingStandImmersive, (config, newVal) -> config.autoCenterBrewingStandImmersive = newVal, this.list);
