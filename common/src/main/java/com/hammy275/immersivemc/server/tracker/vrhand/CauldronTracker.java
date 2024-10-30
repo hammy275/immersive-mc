d23f2cd58d1fe244b86f3081371b44f56a2abdba
hammy275 <hammy275@gmail.com>
2024-10-30 15:44:53 -0400

    Fix Modded Liquid Buckets Crashing Due to My Bad Mixin (#475)

--- common/src/main/java/com/hammy275/immersivemc/server/tracker/vrhand/CauldronTracker.java
+++ common/src/main/java/com/hammy275/immersivemc/server/tracker/vrhand/CauldronTracker.java
@@ -66,13 +66,13 @@ public class CauldronTracker extends AbstractVRHandTracker {
         if (interaction == null) return;
 
         // If holding an empty bucket or glass bottle, see if we can fill it.
-        if (inCauldronBlock && (handItem instanceof BottleItem || (handItem instanceof BucketItem bucketItem && ((BucketItemAccessor) bucketItem).getFluid().isSame(Fluids.EMPTY)))) {
+        if (inCauldronBlock && (handItem instanceof BottleItem || (handItem instanceof BucketItem bucketItem && ((BucketItemAccessor) bucketItem).immersiveMC$getFluid().isSame(Fluids.EMPTY)))) {
             // Pointing up in any way
             if (Math.abs(data.getRoll()) < 90) {
                 possiblySetCooldown(player, interaction.interact(cauldron, player.level(), cauldronPos, player, hand, handStack));
             }
         } else if ((handItem instanceof PotionItem && heldPotion == Potions.WATER) ||
-                (handItem instanceof BucketItem bucketItem && !((BucketItemAccessor) bucketItem).getFluid().isSame(Fluids.EMPTY)) ||
+                (handItem instanceof BucketItem bucketItem && !((BucketItemAccessor) bucketItem).immersiveMC$getFluid().isSame(Fluids.EMPTY)) ||
                 handItem instanceof SolidBucketItem) {
             // 20-degrees in either direction from straight down
             if (Math.abs(data.getRoll()) > 160) {
