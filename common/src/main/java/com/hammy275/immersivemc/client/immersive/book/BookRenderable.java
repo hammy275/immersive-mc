c63d073eaef869816482ac3af1e0bbc32c2dbe51
hammy275 <hammy275@gmail.com>
2024-11-04 15:18:45 -0500

    Book-Type Immersives Can Specify Render Position

--- common/src/main/java/com/hammy275/immersivemc/client/immersive/book/BookRenderable.java
+++ common/src/main/java/com/hammy275/immersivemc/client/immersive/book/BookRenderable.java
@@ -2,6 +2,7 @@ package com.hammy275.immersivemc.client.immersive.book;
 
 import com.hammy275.immersivemc.common.util.PosRot;
 import com.mojang.blaze3d.vertex.PoseStack;
+import net.minecraft.world.phys.Vec3;
 
 /**
  * Something that can be rendered in a book.
@@ -17,4 +18,22 @@ public interface BookRenderable {
      * @param bookPosRot The book's position and rotation.
      */
     public void render(PoseStack stack, ClientBookData data, boolean leftPage, int light, PosRot bookPosRot);
+
+    /**
+     * Called to get the offset on the page to start rendering from. +X is towards the book center, +Y is towards
+     * the top of the page, and +Z is towards the player's face if they're looking directly at the book.
+     * <br>
+     * The coordinate system is described by example below. The rest should be pretty inferrable.
+     * <ul>
+     *     <li>The center of the page is located at (0, 0, 0)</li>
+     *     <li>(1, 0, 0) is the very center of the book</li>
+     *     <li>(0, 1, 0) is the top-center of the page</li>
+     *     <li>(0, 0, 1) is an arbitrary unit closer to the player's face.</li>
+     * </ul>
+     * @param data The book data being rendered.
+     * @param leftPage Whether this is rendering on the left page or the right.
+     * @param bookPosRot The book's position and rotation.
+     * @return The offset to use as described above.
+     */
+    public Vec3 getStartOffset(ClientBookData data, boolean leftPage, PosRot bookPosRot);
 }
