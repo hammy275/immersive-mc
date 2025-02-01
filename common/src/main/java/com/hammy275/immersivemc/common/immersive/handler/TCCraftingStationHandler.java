8839f2382543a218e6578bbd308ed0ee3ebf1f7b
hammy275 <hammy275@gmail.com>
2025-02-01 13:56:42 -0500

    Properly Handle Recipes That Modify Input Slots (#464)

--- common/src/main/java/com/hammy275/immersivemc/common/immersive/handler/TCCraftingStationHandler.java
+++ common/src/main/java/com/hammy275/immersivemc/common/immersive/handler/TCCraftingStationHandler.java
@@ -62,7 +62,8 @@ public class TCCraftingStationHandler extends ContainerHandler<ListOfItemsStorag
             for (int i = 0; i <= 8; i++) {
                 items[i] = table.getItem(i).copy();
             }
-            Swap.handleDoCraft(player, items, pos);
+            items = Swap.handleDoCraft(player, items, pos);
+            if (items == null) return;
             for (int i = 0; i <= 8; i++) {
                 table.setItem(i, items[i]);
             }
