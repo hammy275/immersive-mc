9a30c0bffa49aa6329e9016f3da59e917f6ab700
hammy275 <hammy275@gmail.com>
2025-02-06 16:47:34 -0500

    Crouch Grab for Entire Stack + As Much Craft (and Small SwapTracker Bugfix) (#514)

---
+++ common/src/main/java/com/hammy275/immersivemc/api/common/immersive/SwapMode.java
@@ -0,0 +1,13 @@
+package com.hammy275.immersivemc.api.common.immersive;
+
+/**
+ * The type of swap that should be performed. Found as part of
+ * {@link com.hammy275.immersivemc.api.server.ItemSwapAmount} for how many items to swap into and/or out of an
+ * Immersive.
+ */
+public enum SwapMode {
+    SINGLE, // Only swap one item
+    SPLIT, // Swap the held stack, split evenly into multiple slots
+    ALL, // Swap as many items as possible
+    CONSTANT // Swap a constant number of items
+}
--- common/src/main/java/com/hammy275/immersivemc/client/immersive/ImmersiveBackpack.java
+++ common/src/main/java/com/hammy275/immersivemc/client/immersive/ImmersiveBackpack.java
@@ -12,7 +12,7 @@ import com.hammy275.immersivemc.client.model.BackpackLowDetailModel;
 import com.hammy275.immersivemc.client.model.BackpackModel;
 import com.hammy275.immersivemc.common.config.ActiveConfig;
 import com.hammy275.immersivemc.common.config.PlacementGuideMode;
-import com.hammy275.immersivemc.common.config.PlacementMode;
+import com.hammy275.immersivemc.api.common.immersive.SwapMode;
 import com.hammy275.immersivemc.common.immersive.storage.network.impl.NullStorage;
 import com.hammy275.immersivemc.common.network.Network;
 import com.hammy275.immersivemc.common.network.packet.FetchBackpackStoragePacket;
@@ -98,10 +98,10 @@ public class ImmersiveBackpack extends AbstractPlayerAttachmentImmersive<Backpac
 
     public static void onHitboxInteract(Player player, BackpackInfo info, int slot) {
         if (slot <= 26) { // Inventory handle
-            Network.INSTANCE.sendToServer(new SwapPacket(BlockPos.ZERO, List.of(slot + 9), InteractionHand.MAIN_HAND, PlacementMode.SINGLE, SwapPacket.SwapDestination.INVENTORY));
+            Network.INSTANCE.sendToServer(new SwapPacket(BlockPos.ZERO, List.of(slot + 9), InteractionHand.MAIN_HAND, SwapMode.SINGLE, SwapPacket.SwapDestination.INVENTORY));
             Swap.handleInventorySwap(player, slot + 9, InteractionHand.MAIN_HAND); // Do swap on both sides
         } else {
-            Network.INSTANCE.sendToServer(new SwapPacket(BlockPos.ZERO, List.of(slot), InteractionHand.MAIN_HAND, PlacementMode.SINGLE, SwapPacket.SwapDestination.BAG_CRAFTING));
+            Network.INSTANCE.sendToServer(new SwapPacket(BlockPos.ZERO, List.of(slot), InteractionHand.MAIN_HAND, SwapMode.SINGLE, SwapPacket.SwapDestination.BAG_CRAFTING));
             Network.INSTANCE.sendToServer(new FetchBackpackStoragePacket());
         }
     }
