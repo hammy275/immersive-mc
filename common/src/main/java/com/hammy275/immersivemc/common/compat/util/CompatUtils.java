41c49dd8a42289296721a21aeb7a4de1fe96132a
hammy275 <hammy275@gmail.com>
2025-02-12 19:55:08 -0500

    Iron Furnaces Support Give XP (#519)

--- common/src/main/java/com/hammy275/immersivemc/common/compat/util/CompatUtils.java
+++ common/src/main/java/com/hammy275/immersivemc/common/compat/util/CompatUtils.java
@@ -1,5 +1,9 @@
 package com.hammy275.immersivemc.common.compat.util;
 
+import org.jetbrains.annotations.Nullable;
+
+import java.lang.reflect.Method;
+
 public class CompatUtils {
 
     public static Class<?> getClazz(String classPath) {
@@ -7,6 +11,15 @@ public class CompatUtils {
             return Class.forName(classPath);
         } catch (ClassNotFoundException e) {
             return NullClass.class;
+        }
+    }
+
+    @Nullable
+    public static Method getMethod(Class<?> clazz, String methodName, Class<?>... parameterTypes) {
+        try {
+            return clazz.getMethod(methodName, parameterTypes);
+        } catch (NoSuchMethodException e) {
+            return null;
         }
     }
 }
--- common/src/main/java/com/hammy275/immersivemc/common/immersive/handler/IronFurnacesFurnaceHandler.java
+++ common/src/main/java/com/hammy275/immersivemc/common/immersive/handler/IronFurnacesFurnaceHandler.java
@@ -5,6 +5,8 @@ import com.hammy275.immersivemc.common.compat.IronFurnaces;
 import com.hammy275.immersivemc.common.config.ActiveConfig;
 import net.minecraft.core.BlockPos;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.WorldlyContainer;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 
@@ -23,5 +25,10 @@ public class IronFurnacesFurnaceHandler extends FurnaceHandler {
     @Override
     public ResourceLocation getID() {
         return ResourceLocation.fromNamespaceAndPath(ImmersiveMC.MOD_ID, "iron_furnaces_furnace");
+    }
+
+    @Override
+    protected void awardXP(WorldlyContainer furnace, ServerPlayer player) {
+        IronFurnaces.doUnlockRecipes(furnace, player);
     }
 }
