4bb5f769a64f5f86c8f575071cdedbdbfda27027
hammy275 <hammy275@gmail.com>
2025-02-19 11:12:19 -0500

    Drag Hitbox Clone Over and Smaller Drag Hitbox Size (#521)

--- common/src/main/java/com/hammy275/immersivemc/client/immersive/Immersives.java
+++ common/src/main/java/com/hammy275/immersivemc/client/immersive/Immersives.java
@@ -479,7 +479,6 @@ public class Immersives {
                         ImmersiveClientLogicHelpers.instance().sendSwapPacket(info.getBlockPosition(), slots, hand, modifierPressed);
                         return ClientConstants.defaultCooldownTicks;
                     })
-                    .setNoDragHitbox()
                     .build(),
             BuiltImmersive.class,
             Apoth.compatData
