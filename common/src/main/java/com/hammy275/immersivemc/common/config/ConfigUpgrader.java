ec0db0f0a8ba6b55edeb5b6446631e50c8f42654
hammy275 <hammy275@gmail.com>
2025-02-21 17:27:08 -0500

    Item Guide Presets (#527)

--- common/src/main/java/com/hammy275/immersivemc/common/config/ConfigUpgrader.java
+++ common/src/main/java/com/hammy275/immersivemc/common/config/ConfigUpgrader.java
@@ -1,5 +1,7 @@
 package com.hammy275.immersivemc.common.config;
 
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static com.hammy275.immersivemc.common.config.ActiveConfig.COMMON_CONFIG_VERSION;
@@ -40,6 +42,24 @@ public class ConfigUpgrader {
                 boolean bypassImmersive = (boolean) config.getOrDefault("crouchingBypassesImmersives", false);
                 config.put("crouchMode", bypassImmersive ? CrouchMode.BYPASS_IMMERSIVE.toString() : CrouchMode.SWAP_ALL.toString());
                 config.remove("crouchingBypassesImmersives");
+            } else if (version == 3) {
+                // Convert up colors to modern system
+                int itemGuideColor = readInt(config, "itemGuideColor");
+                int itemGuideSelectedColor = readInt(config, "itemGuideSelectedColor");
+                int rangedGrabColor = readInt(config, "rangedGrabColor");
+                if (itemGuideColor == 0x3300ffff && itemGuideSelectedColor == 0x3300ff00 && rangedGrabColor == 0xff00ffff) {
+                    config.put("itemGuidePreset", ItemGuidePreset.CLASSIC.name());
+                } else if (itemGuideColor == 0x638b8b8b && itemGuideSelectedColor == 0x7fc5c5c5 && rangedGrabColor == 0xffc5c5c5) {
+                    config.put("itemGuidePreset", ItemGuidePreset.GRAY.name());
+                } else if (itemGuideColor != -1 && itemGuideSelectedColor != -1 && rangedGrabColor != -1) {
+                    config.put("itemGuidePreset", ItemGuidePreset.CUSTOM.name());
+                    Map<Object, Object> customColorMap = new HashMap<>();
+                    customColorMap.put("colors", List.of(itemGuideColor));
+                    customColorMap.put("selectedColors", List.of(itemGuideSelectedColor));
+                    customColorMap.put("rangedGrabColors", List.of(rangedGrabColor));
+                    customColorMap.put("transitionTimeMS", 5000);
+                    config.put("itemGuideCustomColorData", customColorMap);
+                }
             }
 
             version++;
