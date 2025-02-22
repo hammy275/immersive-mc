f3614e23dc8f50002715b8bdcff8a2e65a3958e7
hammy275 <hammy275@gmail.com>
2025-02-22 15:19:34 -0500

    Significant Item Guide Preset Optimization (#527)

--- common/src/main/java/com/hammy275/immersivemc/client/config/screen/ItemGuideCustomizeScreen.java
+++ common/src/main/java/com/hammy275/immersivemc/client/config/screen/ItemGuideCustomizeScreen.java
@@ -25,6 +25,7 @@ import net.minecraft.world.phys.AABB;
 import net.minecraft.world.phys.Vec3;
 
 import java.time.Instant;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ItemGuideCustomizeScreen extends OptionsSubScreen {
@@ -199,7 +200,7 @@ public class ItemGuideCustomizeScreen extends OptionsSubScreen {
                         1, 10,
                         colors::size,
                         newVal -> {
-                            List<RGBA> newColors = colors;
+                            List<RGBA> newColors = new ArrayList<>(colors);
                             while (newVal > newColors.size()) {
                                 newColors.add(RGBA.random());
                             }
