beed6a1c58aee2c5faecc460ece806e84b75fa0e
hammy275 <hammy275@gmail.com>
2025-02-28 15:57:39 -0500

    Grab Beacon (#355)

--- common/src/main/java/com/hammy275/immersivemc/client/immersive_item/HandImmersives.java
+++ common/src/main/java/com/hammy275/immersivemc/client/immersive_item/HandImmersives.java
@@ -7,9 +7,11 @@ public class HandImmersives {
 
     public static final List<AbstractHandImmersive<?>> HAND_IMMERSIVES = new ArrayList<>();
 
+    public static final HeldImageImmersive heldImageImmersive = new HeldImageImmersive();
     public static final WrittenBookImmersive writtenBookImmersive = new WrittenBookImmersive();
 
     static {
+        HAND_IMMERSIVES.add(heldImageImmersive);
         HAND_IMMERSIVES.add(writtenBookImmersive);
     }
 }
