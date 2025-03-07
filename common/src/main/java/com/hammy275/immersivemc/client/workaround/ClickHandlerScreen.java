a37edb4bda8c531e06d2d831e054508a2e0ecbd8
hammy275 <hammy275@gmail.com>
2025-03-06 21:10:11 -0500

    Fix URL Opening from Immersive Book

--- common/src/main/java/com/hammy275/immersivemc/client/workaround/ClickHandlerScreen.java
+++ common/src/main/java/com/hammy275/immersivemc/client/workaround/ClickHandlerScreen.java
@@ -1,6 +1,7 @@
 package com.hammy275.immersivemc.client.workaround;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.ClickEvent;
 import net.minecraft.network.chat.Component;
@@ -8,6 +9,9 @@ import net.minecraft.network.chat.Style;
 import org.jetbrains.annotations.Nullable;
 
 public class ClickHandlerScreen extends Screen {
+
+    private boolean closeWhenAble = false;
+
     public ClickHandlerScreen() {
         super(Component.empty());
     }
@@ -18,11 +22,21 @@ public class ClickHandlerScreen extends Screen {
         if (style != null && style.getClickEvent() != null) {
             ClickEvent.Action action = style.getClickEvent().getAction();
             if (action == ClickEvent.Action.RUN_COMMAND || action == ClickEvent.Action.COPY_TO_CLIPBOARD ||
-                action == ClickEvent.Action.SUGGEST_COMMAND || action == ClickEvent.Action.OPEN_URL) {
+                action == ClickEvent.Action.SUGGEST_COMMAND) {
                 // SUGGEST_COMMAND seems to not work in vanilla, so no need to make it work here
                 Minecraft.getInstance().setScreen(null);
+            } else if (action == ClickEvent.Action.OPEN_URL) {
+                this.closeWhenAble = true; // Need to wait until this screen is restored to close
             }
         }
         return ret;
+    }
+
+    @Override
+    public void render(GuiGraphics guiGraphics, int mouseX, int mouseY, float partialTick) {
+        super.render(guiGraphics, mouseX, mouseY, partialTick);
+        if (closeWhenAble) {
+            Minecraft.getInstance().setScreen(null); // Hacky, but Screens don't have a tick()
+        }
     }
 }
