da448b7477aa5474fbc1bec77fe5d213db7df9c8
hammy275 <hammy275@gmail.com>
2025-03-10 16:27:04 -0400

    Ignore Y Vector for Shield Blocking and Better Shield Mixin (#536) (#400)

--- common/src/main/java/com/hammy275/immersivemc/mixin/LivingEntityMixin.java
+++ common/src/main/java/com/hammy275/immersivemc/mixin/LivingEntityMixin.java
@@ -1,42 +1,38 @@
 package com.hammy275.immersivemc.mixin;
 
 import com.hammy275.immersivemc.common.vr.VRPluginVerify;
-import com.hammy275.immersivemc.common.vr.mixin_proxy.LivingEntityMixinProxy;
-import net.minecraft.sounds.SoundEvents;
-import net.minecraft.sounds.SoundSource;
-import net.minecraft.util.Mth;
-import net.minecraft.world.InteractionHand;
+import com.hammy275.immersivemc.common.vr.mixin_proxy.ShieldProxy;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(LivingEntity.class)
 public abstract class LivingEntityMixin {
-
-    @Redirect(method = "hurt(Lnet/minecraft/world/damagesource/DamageSource;F)Z",
-            at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/LivingEntity;isDamageSourceBlocked(Lnet/minecraft/world/damagesource/DamageSource;)Z"))
-    public boolean immersiveMC$isDamageSourceBlocked(LivingEntity shieldHolder, DamageSource damageSource, DamageSource damageSourceAgain, float damage) {
+    @Inject(method = "isDamageSourceBlocked(Lnet/minecraft/world/damagesource/DamageSource;)Z",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/world/phys/Vec3;dot(Lnet/minecraft/world/phys/Vec3;)D"), cancellable = true)
+    public void immersiveMC$isDamageSourceBlocked(DamageSource damageSource, CallbackInfoReturnable<Boolean> cir) {
+        LivingEntity me = (LivingEntity) (Object) this;
         if (VRPluginVerify.hasAPI) {
-            ItemStack stackSource = LivingEntityMixinProxy.isDamageSourceBlocked((LivingEntity) (Object) this,
-                    damageSource);
-            if (stackSource != null) {
-                if (!stackSource.isEmpty() && damage >= 3.0f) {
-                    stackSource.hurtAndBreak(1 + Mth.floor(damage), shieldHolder, (player) -> {
-                        player.broadcastBreakEvent(player.getItemInHand(InteractionHand.MAIN_HAND) == stackSource ?
-                                InteractionHand.MAIN_HAND : InteractionHand.OFF_HAND);
-                        if (!player.level().isClientSide) {
-                            player.level().playSound(null, player.getX(),
-                                    player.getY(), player.getZ(), SoundEvents.SHIELD_BREAK,
-                                    SoundSource.PLAYERS, 0.8F, 0.8F + player.level().random.nextFloat() * 0.4F);
-                        }
-                    });
-                }
-                return !stackSource.isEmpty();
+            boolean doBlock = ShieldProxy.isDamageSourceBlocked(me, damageSource);
+            if (doBlock) {
+                cir.setReturnValue(true);
+                return;
             }
         }
-        return shieldHolder.isDamageSourceBlocked(damageSource);
+        // Since we're forcing Minecraft to assume we're blocking with a shield, we should tell it we're not actually
+        // blocking if there's no use item.
+        if (me.getUseItem().isEmpty()) {
+            cir.setReturnValue(false);
+        }
+    }
+
+    @Inject(method = "isBlocking", at = @At("RETURN"), cancellable = true)
+    public void immersiveMC$injectShieldAsItemBlockingWith(CallbackInfoReturnable<Boolean> cir) {
+        if (VRPluginVerify.hasAPI && !cir.getReturnValue()) {
+            cir.setReturnValue(ShieldProxy.getABlockingShield((LivingEntity) (Object) this) != null);
+        }
     }
 }
