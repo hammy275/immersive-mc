8ae8870b01c4dc082a6d0cf6f2df449b4cf2dbac
hammy275 <hammy275@gmail.com>
2025-03-10 16:27:04 -0400

    Ignore Y Vector for Shield Blocking and Better Shield Mixin (#536) (#400)

---
+++ common/src/main/java/com/hammy275/immersivemc/common/vr/mixin_proxy/ShieldProxy.java
@@ -0,0 +1,61 @@
+package com.hammy275.immersivemc.common.vr.mixin_proxy;
+
+import com.hammy275.immersivemc.common.config.ActiveConfig;
+import com.hammy275.immersivemc.common.vr.VRPlugin;
+import net.blf02.vrapi.api.data.IVRData;
+import net.blf02.vrapi.api.data.IVRPlayer;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.UseAnim;
+import net.minecraft.world.phys.Vec3;
+import org.jetbrains.annotations.Nullable;
+
+public class ShieldProxy {
+
+    public static ItemStack shieldToDamage = ItemStack.EMPTY;
+
+    @Nullable
+    public static ItemStack getABlockingShield(LivingEntity living) {
+        if (living instanceof Player player &&
+                ActiveConfig.getActiveConfigCommon(player).useShieldImmersive && VRPlugin.API.playerInVR(player)
+                && player.getUseItem().isEmpty()) {
+            for (InteractionHand iHand : InteractionHand.values()) {
+                if (player.getItemInHand(iHand).getUseAnimation() == UseAnim.BLOCK) {
+                    return player.getItemInHand(iHand);
+                }
+            }
+        }
+        return null;
+    }
+
+    public static boolean isDamageSourceBlocked(LivingEntity living, DamageSource damageSource) {
+        if (living instanceof Player player && ActiveConfig.getActiveConfigCommon(player).useShieldImmersive &&
+                VRPlugin.API.playerInVR(player) && player.getUseItem().isEmpty()) {
+            IVRPlayer vrPlayer = VRPlugin.API.getVRPlayer(player);
+            for (InteractionHand iHand : InteractionHand.values()) {
+                if (player.getItemInHand(iHand).getUseAnimation() == UseAnim.BLOCK) {
+                    // Iterate again if shield is on cooldown
+                    if (player.getCooldowns().isOnCooldown(player.getItemInHand(iHand).getItem())) {
+                        continue;
+                    }
+                    // Multiplier based on left-handedness and based on which hand we're using
+                    float negMult = VRPlugin.API.isLeftHanded(player) ? -1 : 1;
+                    negMult = iHand == InteractionHand.MAIN_HAND ? negMult * -1 : negMult * 1;
+                    IVRData hand = vrPlayer.getController(iHand.ordinal());
+                    float toRot = (float) (Math.PI / 2f * negMult);
+                    Vec3 handVec = hand.getLookAngle().yRot(toRot).normalize();
+                    Vec3 attackerVec = damageSource.getSourcePosition().vectorTo(player.position()).multiply(1, 0, 1).normalize();
+                    double angle = Math.acos(handVec.dot(attackerVec)); // Angle in radians
+                    if (angle <= Math.PI && angle >= 2 * Math.PI / 3) { // 60 degrees in each direction from shield vec
+                        shieldToDamage = player.getItemInHand(iHand);
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+}
