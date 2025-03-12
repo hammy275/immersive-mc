7831e197c89714dc1a823023088a524eae968f1c
hammy275 <hammy275@gmail.com>
2025-03-12 16:10:31 -0400

    Command Single-Target + Proper Erroring

--- common/src/main/java/com/hammy275/immersivemc/server/command/ImmersiveMCCommand.java
+++ common/src/main/java/com/hammy275/immersivemc/server/command/ImmersiveMCCommand.java
@@ -10,8 +10,6 @@ import net.minecraft.commands.arguments.EntityArgument;
 import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerPlayer;
 
-import java.util.Collection;
-
 public class ImmersiveMCCommand {
 
     public static void createCommand(CommandDispatcher<CommandSourceStack> dispatcher) {
@@ -21,33 +19,37 @@ public class ImmersiveMCCommand {
                         .then(
                                 Commands.literal("enable")
                                         .then(
-                                                Commands.argument("players", EntityArgument.players())
+                                                Commands.argument("player", EntityArgument.player())
                                                         .executes(context ->
-                                                                enableDisable(context.getSource(), EntityArgument.getPlayers(context, "players"), true))
+                                                                enableDisable(context.getSource(), EntityArgument.getPlayer(context, "player"), true))
                                         )
                         )
                         .then(
                                 Commands.literal("disable")
                                         .then(
-                                                Commands.argument("players", EntityArgument.players())
+                                                Commands.argument("player", EntityArgument.player())
                                                         .executes(context ->
-                                                                enableDisable(context.getSource(), EntityArgument.getPlayers(context, "players"), false))
+                                                                enableDisable(context.getSource(), EntityArgument.getPlayer(context, "player"), false))
                                         )
                         )
         );
     }
 
-    private static int enableDisable(CommandSourceStack source, Collection<ServerPlayer> targets, boolean nowEnabled) {
-        for (ServerPlayer player : targets) {
-            if (nowEnabled) {
-                ImmersiveMCPlayerStorages.setPlayerEnabled(player);
-            } else {
-                ImmersiveMCPlayerStorages.setPlayerDisabled(player);
-            }
-            ConfigSyncPacket.syncConfigToPlayer(player);
+    private static int enableDisable(CommandSourceStack source, ServerPlayer player, boolean nowEnabled) {
+        boolean currentlyEnabled = !ImmersiveMCPlayerStorages.isPlayerDisabled(player);
+        if (currentlyEnabled == nowEnabled) {
+            source.sendFailure(Component.translatableWithFallback("commands." + ImmersiveMC.MOD_ID + ".enable_disable.already_" + nowEnabled,
+                    "This player already has ImmersiveMC %s!".formatted(nowEnabled ? "enabled" : "disabled")));
+            return 0;
         }
+        if (nowEnabled) {
+            ImmersiveMCPlayerStorages.setPlayerEnabled(player);
+        } else {
+            ImmersiveMCPlayerStorages.setPlayerDisabled(player);
+        }
+        ConfigSyncPacket.syncConfigToPlayer(player);
         source.sendSuccess(() -> Component.translatableWithFallback("commands." + ImmersiveMC.MOD_ID + ".enable_disable." + nowEnabled,
-                "ImmersiveMC now %s for the provided players!".formatted(nowEnabled ? "enabled" : "disabled")), true);
-        return targets.size();
+                "ImmersiveMC now %s for the provided player!".formatted(nowEnabled ? "enabled" : "disabled")), true);
+        return 1;
     }
 }
