21d165eeb4f6b2be55aaba923d07861be76d2832
hammy275 <hammy275@gmail.com>
2025-03-12 21:29:56 -0400

    Disable Auto Step Up for Immersive Blocks (#305)

---
+++ common/src/main/java/com/hammy275/immersivemc/client/vr/ClientMixinProxy.java
@@ -0,0 +1,26 @@
+package com.hammy275.immersivemc.client.vr;
+
+import com.hammy275.immersivemc.common.config.ActiveConfig;
+import com.hammy275.immersivemc.common.util.Util;
+import com.hammy275.immersivemc.common.vr.VRPluginVerify;
+import net.minecraft.client.multiplayer.ClientLevel;
+import net.minecraft.client.player.LocalPlayer;
+import net.minecraft.core.BlockPos;
+import net.minecraft.util.Mth;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.phys.AABB;
+
+import java.util.stream.StreamSupport;
+
+public class ClientMixinProxy {
+    public static boolean collideDoPlayerMoveInRoomRedirect(ClientLevel level, Entity entity, AABB aabb) {
+        if (VRPluginVerify.clientInVR() && ActiveConfig.getActiveConfigCommon((LocalPlayer) entity).dontAutoStepOnImmersiveBlocksInVR) {
+            if (StreamSupport.stream(BlockPos.betweenClosed(Mth.floor(aabb.minX), Mth.floor(aabb.minY - 0.500001), Mth.floor(aabb.minZ),
+                            Mth.floor(aabb.maxX), Mth.floor(aabb.maxY), Mth.floor(aabb.maxZ)).spliterator(), false)
+                    .anyMatch(pos -> Util.blockIsActiveImmersive((LocalPlayer) entity, pos))) {
+                return false; // Collide if trying to move on top of an Immersive block
+            }
+        }
+        return level.noCollision(entity, aabb);
+    }
+}
---
+++ common/src/main/java/com/hammy275/immersivemc/mixin/VRPlayerMixinNewVivecraft.java
@@ -0,0 +1,21 @@
+package com.hammy275.immersivemc.mixin;
+
+import com.hammy275.immersivemc.client.vr.ClientMixinProxy;
+import net.minecraft.client.multiplayer.ClientLevel;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.phys.AABB;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Pseudo;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Pseudo
+@Mixin(targets = "org.vivecraft.client_vr.gameplay.VRPlayer")
+public class VRPlayerMixinNewVivecraft {
+
+    @Redirect(method = "doPlayerMoveInRoom",
+            at = @At(value = "INVOKE", ordinal = 2, target = "Lnet/minecraft/client/multiplayer/ClientLevel;noCollision(Lnet/minecraft/world/entity/Entity;Lnet/minecraft/world/phys/AABB;)Z"))
+    private boolean immersiveMC$collideIfWouldBeAboveImmersiveBlock(ClientLevel level, Entity entity, AABB aabb) {
+        return ClientMixinProxy.collideDoPlayerMoveInRoomRedirect(level, entity, aabb);
+    }
+}
