b1d1a357f245b3a1722baf83f3ed75188bfdf261
hammy275 <hammy275@gmail.com>
2025-03-12 21:29:56 -0400

    Disable Auto Step Up for Immersive Blocks (#305)

---
+++ common/src/main/java/com/hammy275/immersivemc/mixin/VRPlayerMixin.java
@@ -0,0 +1,20 @@
+package com.hammy275.immersivemc.mixin;
+
+import com.hammy275.immersivemc.client.vr.ClientMixinProxy;
+import net.minecraft.client.multiplayer.ClientLevel;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.phys.AABB;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import org.vivecraft.gameplay.VRPlayer;
+
+@Mixin(VRPlayer.class)
+public class VRPlayerMixin {
+
+    @Redirect(method = "doPlayerMoveInRoom",
+            at = @At(value = "INVOKE", ordinal = 2, target = "Lnet/minecraft/client/multiplayer/ClientLevel;noCollision(Lnet/minecraft/world/entity/Entity;Lnet/minecraft/world/phys/AABB;)Z"))
+    private boolean immersiveMC$collideIfWouldBeAboveImmersiveBlock(ClientLevel level, Entity entity, AABB aabb) {
+        return ClientMixinProxy.collideDoPlayerMoveInRoomRedirect(level, entity, aabb);
+    }
+}
