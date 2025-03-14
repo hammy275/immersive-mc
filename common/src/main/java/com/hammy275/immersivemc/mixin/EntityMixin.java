d44b0d61ea66a573ab783494b3c6b5c8f9fb3e1d
hammy275 <hammy275@gmail.com>
2025-03-14 18:15:43 -0400

    Better Auto Step Up Prevention on Older Minecraft Versions (#305)

--- common/src/main/java/com/hammy275/immersivemc/mixin/EntityMixin.java
+++ common/src/main/java/com/hammy275/immersivemc/mixin/EntityMixin.java
@@ -4,34 +4,40 @@ import com.hammy275.immersivemc.common.config.ActiveConfig;
 import com.hammy275.immersivemc.common.util.Util;
 import com.hammy275.immersivemc.common.vr.VRPluginVerify;
 import net.minecraft.core.BlockPos;
-import net.minecraft.util.Mth;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.phys.AABB;
 import net.minecraft.world.phys.Vec3;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.stream.StreamSupport;
 
 @Mixin(Entity.class)
-public class EntityMixin {
+public abstract class EntityMixin {
 
-    @Redirect(method = "move",
-        at = @At(value = "INVOKE", ordinal = 1, target = "Lnet/minecraft/world/phys/Vec3;lengthSqr()D"))
-    private double immersiveMC$cancelMovementIfMovingUpToAnImmersiveBlock(Vec3 moveVec) {
-        // If we're going up, any block in our new hitbox or any block -0.500001 blocks below our hitbox contains an
-        // active Immersive, and we aren't jumping, don't move.
-        if (moveVec.y >= 0.125 && (Object) this instanceof Player player && ActiveConfig.getActiveConfigCommon(player).dontAutoStepOnImmersiveBlocksInVR
-            && VRPluginVerify.playerInVR(player) && !((LivingEntityAccessor) this).immersiveMC$jumping()) {
-            AABB aabb = player.getBoundingBox().move(moveVec);
-            if (StreamSupport.stream(BlockPos.betweenClosed(Mth.floor(aabb.minX), Mth.floor(aabb.minY - 0.500001), Mth.floor(aabb.minZ),
-                            Mth.floor(aabb.maxX), Mth.floor(aabb.maxY), Mth.floor(aabb.maxZ)).spliterator(), false)
+    @Shadow protected abstract Vec3 collide(Vec3 vec);
+
+    @Shadow public float maxUpStep;
+
+    @Inject(method = "collide", at = @At("RETURN"), cancellable = true)
+    private void immersiveMC$differentCollideIfSteppingOntoImemrsive(Vec3 moveVecIn, CallbackInfoReturnable<Vec3> cir) {
+        if (maxUpStep > 0.0001f && (Object) this instanceof Player player && ActiveConfig.getActiveConfigCommon(player).dontAutoStepOnImmersiveBlocksInVR
+                && VRPluginVerify.playerInVR(player) && !((LivingEntityAccessor) this).immersiveMC$jumping()) {
+            if (StreamSupport.stream(player.level.getBlockCollisions(player, player.getBoundingBox().expandTowards(moveVecIn)).spliterator(), false)
+                    .filter(shape -> !shape.isEmpty())
+                    .map(shape -> BlockPos.containing(shape.bounds().getCenter()))
                     .anyMatch(pos -> Util.blockIsActiveImmersive(player, pos))) {
-                return 0;
+                Vec3 moveVecOut = cir.getReturnValue();
+                if (moveVecOut.y > 0) {
+                    float oldMaxUpStep = this.maxUpStep;
+                    this.maxUpStep = 0.0001f;
+                    cir.setReturnValue(this.collide(moveVecIn));
+                    this.maxUpStep = oldMaxUpStep;
+                }
             }
         }
-        return moveVec.lengthSqr();
     }
 }
