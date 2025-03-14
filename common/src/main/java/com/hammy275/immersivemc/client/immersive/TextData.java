b72f1784e36d0ca03d788d86a7bee5a3ed7a6d12
hammy275 <hammy275@gmail.com>
2025-03-13 20:44:44 -0400

    Lerp Rendering for Things in Motion (#544)

--- common/src/main/java/com/hammy275/immersivemc/client/immersive/TextData.java
+++ common/src/main/java/com/hammy275/immersivemc/client/immersive/TextData.java
@@ -3,5 +3,5 @@ package com.hammy275.immersivemc.client.immersive;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.phys.Vec3;
 
-public record TextData(Component text, Vec3 pos) {
+public record TextData(Component text, Vec3 offset) {
 }
