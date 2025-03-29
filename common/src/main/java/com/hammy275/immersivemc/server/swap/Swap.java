40dedd6472a976c93e25f52f874f2b08819f8baa
hammy275 <hammy275@gmail.com>
2025-03-29 11:43:13 -0400

    Use InventoryMenu for Bag Crafting

--- common/src/main/java/com/hammy275/immersivemc/server/swap/Swap.java
+++ common/src/main/java/com/hammy275/immersivemc/server/swap/Swap.java
@@ -31,7 +31,9 @@ import net.minecraft.world.inventory.ContainerLevelAccess;
 import net.minecraft.world.inventory.CraftingContainer;
 import net.minecraft.world.inventory.CraftingMenu;
 import net.minecraft.world.inventory.EnchantmentMenu;
+import net.minecraft.world.inventory.InventoryMenu;
 import net.minecraft.world.inventory.ItemCombinerMenu;
+import net.minecraft.world.inventory.RecipeBookMenu;
 import net.minecraft.world.inventory.SmithingMenu;
 import net.minecraft.world.inventory.TransientCraftingContainer;
 import net.minecraft.world.item.ItemStack;
@@ -265,7 +267,7 @@ public class Swap {
     public static ItemStack[] handleDoCraft(ServerPlayer player, ItemStack[] stacksIn,
                                      BlockPos tablePos, ItemSwapAmount amount) {
         boolean isBackpack = stacksIn.length == 5;
-        CraftingMenu menu = new CraftingMenu(-1, player.getInventory());
+        RecipeBookMenu<CraftingContainer> menu = isBackpack ? new InventoryMenu(player.getInventory(), false, player) : new CraftingMenu(-1, player.getInventory());
         ItemStack stackOut = getRecipeOutput(player, stacksIn);
         ItemStack firstOut = stackOut;
         ItemStack[] newSlotsState = Arrays.copyOf(stacksIn, stacksIn.length);
@@ -289,6 +291,14 @@ public class Swap {
             stacksToGive.add(stackOut);
             stackOut = getRecipeOutput(player, newSlotsState);
             newSlotsState[newSlotsState.length - 1] = stackOut;
+        }
+
+        // Clear crafting slots to prevent showing output in actual inventory
+        if (isBackpack) {
+            menu.setItem(InventoryMenu.RESULT_SLOT, 0, ItemStack.EMPTY);
+            for (int i = InventoryMenu.CRAFT_SLOT_START; i < InventoryMenu.CRAFT_SLOT_END; i++) {
+                menu.setItem(i, 0, ItemStack.EMPTY);
+            }
         }
 
         if (stacksToGive.isEmpty()) {
