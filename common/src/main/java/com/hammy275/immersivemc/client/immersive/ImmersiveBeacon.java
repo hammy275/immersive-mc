55e777c70e7058faf4b52f626f7422dfa6feaca2
hammy275 <hammy275@gmail.com>
2025-06-18 14:56:30 -0400

    Organize resources Folder (#586)

--- common/src/main/java/com/hammy275/immersivemc/client/immersive/ImmersiveBeacon.java
+++ common/src/main/java/com/hammy275/immersivemc/client/immersive/ImmersiveBeacon.java
@@ -60,8 +60,8 @@ public class ImmersiveBeacon extends AbstractImmersive<BeaconInfo, BeaconStorage
             new ResourceLocation("textures/mob_effect/strength.png")
     };
     private static final ResourceLocation regenerationLocation = new ResourceLocation("textures/mob_effect/regeneration.png");
-    private static final ResourceLocation confirmLocation = new ResourceLocation(ImmersiveMC.MOD_ID, "confirm.png");
-    private static final ResourceLocation addLocation = new ResourceLocation(ImmersiveMC.MOD_ID, "add.png");
+    private static final ResourceLocation confirmLocation = new ResourceLocation(ImmersiveMC.MOD_ID, "immersive/beacon/confirm.png");
+    private static final ResourceLocation addLocation = new ResourceLocation(ImmersiveMC.MOD_ID, "immersive/beacon/add.png");
 
     @Override
     public BeaconInfo buildInfo(BlockPos pos, Level level) {
