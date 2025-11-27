e07724d3977f6566a3efcd3ebd5f9c320fac9b75
hammy275 <hammy275@gmail.com>
2025-11-26 21:35:50 -0500

    Petting API + Allow Petting any Tamed Mob by Default

--- common/src/main/java/com/hammy275/immersivemc/server/tracker/vrhand/PetTracker.java
+++ common/src/main/java/com/hammy275/immersivemc/server/tracker/vrhand/PetTracker.java
@@ -1,21 +1,13 @@
 package com.hammy275.immersivemc.server.tracker.vrhand;
 
+import com.hammy275.immersivemc.api.common.immersive.petting.PettingHandler;
+import com.hammy275.immersivemc.common.api_impl.ImmersiveMCRegistrationImpl;
 import com.hammy275.immersivemc.common.config.ActiveConfig;
 import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.server.level.ServerLevel;
-import net.minecraft.sounds.SoundEvent;
-import net.minecraft.sounds.SoundEvents;
-import net.minecraft.sounds.SoundSource;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.TamableAnimal;
-import net.minecraft.world.entity.animal.Cat;
-import net.minecraft.world.entity.animal.Wolf;
-import net.minecraft.world.entity.animal.horse.AbstractHorse;
-import net.minecraft.world.entity.animal.horse.Donkey;
-import net.minecraft.world.entity.animal.horse.Horse;
-import net.minecraft.world.entity.animal.horse.Mule;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.phys.AABB;
@@ -25,47 +17,46 @@ import org.vivecraft.api.data.VRBodyPart;
 import org.vivecraft.api.data.VRPose;
 import org.vivecraft.api.data.VRPoseHistory;
 
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class PetTracker extends AbstractVRHandTracker {
 
     public static final double THRESHOLD = 0.02;
+    private static final List<PettingHandler<?>> PETTING_HANDLERS = new ArrayList<>();
+
+    private final Map<UUID, Integer> cooldown = new HashMap<>();
+
+    static {
+        ImmersiveMCRegistrationImpl.doPettingHandlerRegistration(PETTING_HANDLERS::add);
+    }
+
+    public void globalTick() {
+        for (Map.Entry<UUID, Integer> entry : cooldown.entrySet()) {
+            entry.setValue(entry.getValue() - 1);
+        }
+        cooldown.entrySet().removeIf(entry -> entry.getValue() <= 0);
+    }
 
     @Override
     protected boolean shouldRunForHand(Player player, InteractionHand hand, ItemStack stackInHand, VRPose currentVRPose) {
-        return this.getPlayerPetsNearby(player).size() > 0;
+        return VRAPI.instance().getHistoricalVRPoses(player) != null && ThreadLocalRandom.current().nextInt(20) == 0;
     }
 
     @Override
     protected void runForHand(Player player, InteractionHand hand, ItemStack stackInHand, VRPose currentVRData) {
-        if (ThreadLocalRandom.current().nextInt(20) == 0) {
-            for (LivingEntity entity : this.getPlayerPetsNearby(player)) {
-                if (entity.getBoundingBox().inflate(0.2).contains(currentVRData.getHand(hand).getPos())) {
-                    VRPoseHistory history = VRAPI.instance().getHistoricalVRPoses(player);
-                    if (history == null) return;
-                    if (history.averageSpeed(VRBodyPart.fromInteractionHand(hand), 1) >= THRESHOLD) {
-                        ServerLevel level = (ServerLevel) player.level();
-                        Vec3 pos = currentVRData.getHand(hand).getPos();
-                        level.sendParticles(ParticleTypes.HEART, pos.x, pos.y, pos.z, ThreadLocalRandom.current().nextInt(5) + 1,
-                                0.25, 0.1, 0.25, 0.00001);
-                        if (ThreadLocalRandom.current().nextInt(5) == 0) {
-                            SoundEvent sound = null;
-                            if (entity instanceof Wolf) {
-                                sound = SoundEvents.WOLF_PANT;
-                            } else if (entity instanceof Cat) {
-                                sound = SoundEvents.CAT_PURREOW;
-                            } else if (entity instanceof Horse) {
-                                sound = SoundEvents.HORSE_AMBIENT;
-                            } else if (entity instanceof Donkey) {
-                                sound = SoundEvents.DONKEY_AMBIENT;
-                            } else if (entity instanceof Mule) {
-                                sound = SoundEvents.MULE_AMBIENT;
-                            }
-                            if (sound != null) {
-                                level.playSound(null, entity, sound, SoundSource.NEUTRAL, 1f, 1f);
-                            }
+        VRPoseHistory history = VRAPI.instance().getHistoricalVRPoses(player);
+        for (PetInfo<?> info : this.getPlayerPetsNearby((ServerPlayer) player)) {
+            if (info.pet.getBoundingBox().inflate(0.2).contains(currentVRData.getHand(hand).getPos())) {
+                if (history.averageSpeed(VRBodyPart.fromInteractionHand(hand), 1) >= THRESHOLD) {
+                    ServerLevel level = (ServerLevel) player.level();
+                    Vec3 pos = currentVRData.getHand(hand).getPos();
+                    level.sendParticles(ParticleTypes.HEART, pos.x, pos.y, pos.z, ThreadLocalRandom.current().nextInt(5) + 1,
+                            0.25, 0.1, 0.25, 0.00001);
+                    if (!cooldown.containsKey(info.pet.getUUID())) {
+                        int cooldown = info.handlePet((ServerPlayer) player, pos);
+                        if (cooldown > 0) {
+                            this.cooldown.put(info.pet.getUUID(), cooldown);
                         }
                     }
                 }
@@ -78,24 +69,29 @@ public class PetTracker extends AbstractVRHandTracker {
         return config.allowPetting;
     }
 
-    protected List<LivingEntity> getPlayerPetsNearby(Player player) {
-        List<LivingEntity> pets = new LinkedList<>();
+    protected List<PetInfo<?>> getPlayerPetsNearby(ServerPlayer player) {
+        List<PetInfo<?>> pets = new ArrayList<>();
         List<Entity> ents = player.level().getEntities(player, AABB.ofSize(player.position(), 10, 10, 10));
         for (Entity e : ents) {
-            if (ActiveConfig.getConfigForPlayer(player).allowPettingAnythingLiving) {
-                if (e instanceof LivingEntity le) {
-                    pets.add(le);
-                }
-            } else if (e instanceof TamableAnimal ta) {
-                if (ta.getOwner() == player) {
-                    pets.add(ta);
-                }
-            } else if (e instanceof AbstractHorse horse) {
-                if (horse.isTamed()) {
-                    pets.add(horse);
+            for (PettingHandler<?> handler : PETTING_HANDLERS) {
+                if (handler.matchesEntity(player, e)) {
+                    pets.add(PetInfo.create(e, handler));
+                    break;
                 }
             }
         }
         return pets;
+    }
+
+    protected record PetInfo<P extends Entity>(P pet, PettingHandler<P> handler) {
+
+        @SuppressWarnings("unchecked")
+        public static <P extends Entity> PetInfo<P> create(Entity entity, PettingHandler<P> handler) {
+            return new PetInfo<>((P) entity, handler);
+        }
+
+        public int handlePet(ServerPlayer petter, Vec3 handPos) {
+            return handler.doPetEffect(petter, pet, handPos);
+        }
     }
 }
