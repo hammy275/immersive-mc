508039decd448e40957a613744bccbe84ae1ac17
hammy275 <hammy275@gmail.com>
2025-12-26 18:59:38 -0500

    Simplify BackpackTrackerMixin, Fixing a Crash

--- common/src/main/java/com/hammy275/immersivemc/mixin/BackpackTrackerMixin.java
+++ common/src/main/java/com/hammy275/immersivemc/mixin/BackpackTrackerMixin.java
@@ -3,18 +3,14 @@ package com.hammy275.immersivemc.mixin;
 import com.hammy275.immersivemc.common.config.ActiveConfig;
 import net.minecraft.client.player.LocalPlayer;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Pseudo;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import org.vivecraft.client_vr.gameplay.trackers.BackpackTracker;
 
-// targets is the post-hotswitch version
-@Pseudo // Allows compiling even though targets can't be found due to being in hotswitch version
-@Mixin(value = BackpackTracker.class, targets = "org.vivecraft.client_vr.gameplay.trackers.BackpackTracker")
+@Mixin(value = BackpackTracker.class)
 public class BackpackTrackerMixin {
-    @Inject(method = "isActive(Lnet/minecraft/client/player/LocalPlayer;)Z", at = @At("HEAD"), cancellable = true,
-    require = 1)
+    @Inject(method = "isActive", at = @At("HEAD"), cancellable = true, require = 1)
     public void immersiveMC$notActiveIfUsingBagPreHotswitch(LocalPlayer p, CallbackInfoReturnable<Boolean> cir) {
         if (ActiveConfig.active().reachBehindBagMode.usesOverShoulder()) {
             cir.setReturnValue(false);
