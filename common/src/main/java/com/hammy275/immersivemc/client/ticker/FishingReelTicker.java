f222afe7833d19a5980618ee488baf8179256a17
hammy275 <hammy275@gmail.com>
2026-01-25 16:57:15 -0500

    Refactor Trackers and Rename to Tickers (#457)

--- common/src/main/java/com/hammy275/immersivemc/client/ticker/FishingReelTicker.java
+++ common/src/main/java/com/hammy275/immersivemc/client/ticker/FishingReelTicker.java
@@ -1,8 +1,9 @@
-package com.hammy275.immersivemc.client.tracker.vr;
+package com.hammy275.immersivemc.client.ticker;
 
 import com.hammy275.immersivemc.common.config.ActiveConfig;
 import com.hammy275.immersivemc.common.network.Network;
 import com.hammy275.immersivemc.common.network.packet.ReelFishPacket;
+import com.hammy275.immersivemc.common.ticker.AbstractTicker;
 import com.hammy275.immersivemc.common.vr.VRUtil;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.entity.player.Player;
@@ -10,17 +11,21 @@ import net.minecraft.world.item.FishingRodItem;
 import net.minecraft.world.phys.Vec3;
 import org.vivecraft.api.VRAPI;
 import org.vivecraft.api.data.VRBodyPart;
+import org.vivecraft.api.data.VRPose;
+import org.vivecraft.api.data.VRPoseHistory;
 
-public class FishingReelTrackerCore {
+public class FishingReelTicker extends AbstractTicker {
 
-    public static void tick(Player player) {
+    @Override
+    protected void tick(Player player, VRPose pose, VRPoseHistory poseHistory) {
         Vec3 vel = VRUtil.changeForVelocity(player, VRBodyPart.fromInteractionHand(InteractionHand.MAIN_HAND));
         if (vel.lengthSqr() >= 0.175 && vel.y > 0) {
             Network.INSTANCE.sendToServer(new ReelFishPacket());
         }
     }
 
-    public static boolean shouldTick(Player player) {
+    @Override
+    protected boolean shouldTick(Player player, VRPose pose, VRPoseHistory poseHistory) {
         return ActiveConfig.active().useThrowingImmersive && VRAPI.instance().isVRPlayer(player) &&
                 player.getItemInHand(InteractionHand.MAIN_HAND).getItem() instanceof FishingRodItem &&
                 player.fishing != null;
--- common/src/main/java/com/hammy275/immersivemc/server/ticker/hand/PetTicker.java
+++ common/src/main/java/com/hammy275/immersivemc/server/ticker/hand/PetTicker.java
@@ -1,63 +1,46 @@
-package com.hammy275.immersivemc.server.tracker.vrhand;
+package com.hammy275.immersivemc.server.ticker.hand;
 
 import com.hammy275.immersivemc.api.common.immersive.petting.PettingHandler;
 import com.hammy275.immersivemc.common.api_impl.ImmersiveMCRegistrationImpl;
 import com.hammy275.immersivemc.common.config.ActiveConfig;
+import com.hammy275.immersivemc.common.ticker.AbstractHandTicker;
 import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.phys.AABB;
 import net.minecraft.world.phys.Vec3;
-import org.vivecraft.api.VRAPI;
 import org.vivecraft.api.data.VRBodyPart;
-import org.vivecraft.api.data.VRPose;
+import org.vivecraft.api.data.VRBodyPartData;
 import org.vivecraft.api.data.VRPoseHistory;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 
-public class PetTracker extends AbstractVRHandTracker {
+public class PetTicker extends AbstractHandTicker {
 
     public static final double THRESHOLD = 0.02;
     private static final List<PettingHandler<?>> PETTING_HANDLERS = new ArrayList<>();
-
-    private final Map<UUID, Integer> cooldown = new HashMap<>();
-
+    
     static {
         ImmersiveMCRegistrationImpl.doPettingHandlerRegistration(PETTING_HANDLERS::add);
     }
 
-    public void globalTick() {
-        for (Map.Entry<UUID, Integer> entry : cooldown.entrySet()) {
-            entry.setValue(entry.getValue() - 1);
-        }
-        cooldown.entrySet().removeIf(entry -> entry.getValue() <= 0);
-    }
-
     @Override
-    protected boolean shouldRunForHand(Player player, InteractionHand hand, ItemStack stackInHand, VRPose currentVRPose) {
-        return VRAPI.instance().getHistoricalVRPoses(player) != null && ThreadLocalRandom.current().nextInt(20) == 0;
-    }
-
-    @Override
-    protected void runForHand(Player player, InteractionHand hand, ItemStack stackInHand, VRPose currentVRData) {
-        VRPoseHistory history = VRAPI.instance().getHistoricalVRPoses(player);
+    protected void tickHand(Player player, InteractionHand hand, VRBodyPartData handData, VRPoseHistory poseHistory) {
         for (PetInfo<?> info : this.getPlayerPetsNearby((ServerPlayer) player)) {
-            if (info.pet.getBoundingBox().inflate(0.2).contains(currentVRData.getHand(hand).getPos())) {
-                if (history.averageSpeed(VRBodyPart.fromInteractionHand(hand), 1) >= THRESHOLD) {
+            if (info.pet.getBoundingBox().inflate(0.2).contains(handData.getPos())) {
+                if (poseHistory.averageSpeed(VRBodyPart.fromInteractionHand(hand), 1) >= THRESHOLD) {
                     ServerLevel level = (ServerLevel) player.level;
-                    Vec3 pos = currentVRData.getHand(hand).getPos();
+                    Vec3 pos = handData.getPos();
                     level.sendParticles(ParticleTypes.HEART, pos.x, pos.y, pos.z, ThreadLocalRandom.current().nextInt(5) + 1,
                             0.25, 0.1, 0.25, 0.00001);
-                    if (!cooldown.containsKey(info.pet.getUUID())) {
-                        int cooldown = info.handlePet((ServerPlayer) player, pos);
-                        if (cooldown > 0) {
-                            this.cooldown.put(info.pet.getUUID(), cooldown);
-                        }
+                    int cooldown = info.handlePet((ServerPlayer) player, pos);
+                    if (cooldown > 0) {
+                        setCooldown(player, cooldown);
                     }
                 }
             }
@@ -65,8 +48,8 @@ public class PetTracker extends AbstractVRHandTracker {
     }
 
     @Override
-    public boolean isEnabledInConfig(ActiveConfig config) {
-        return config.allowPetting;
+    protected boolean shouldTickHand(Player player, InteractionHand hand, VRBodyPartData handData, VRPoseHistory poseHistory) {
+        return ActiveConfig.getConfigForPlayer(player).allowPetting && ThreadLocalRandom.current().nextInt(20) == 0;
     }
 
     protected List<PetInfo<?>> getPlayerPetsNearby(ServerPlayer player) {
