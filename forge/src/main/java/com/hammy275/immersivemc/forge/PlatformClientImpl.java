f94aacd8ffdc00775634f5c2fffeebb4e5f2cb59
hammy275 <hammy275@gmail.com>
2026-02-02 17:46:29 -0500

    Fix Disconnect Event Call on Join (#631)

--- forge/src/main/java/com/hammy275/immersivemc/forge/PlatformClientImpl.java
+++ forge/src/main/java/com/hammy275/immersivemc/forge/PlatformClientImpl.java
@@ -29,7 +29,9 @@ public class PlatformClientImpl {
     }
     public static void registerOnClientDisconnectListener(Consumer<Player> listener) {
         MinecraftForge.EVENT_BUS.addListener((ClientPlayerNetworkEvent.LoggingOut event) -> {
-            listener.accept(event.getPlayer());
+            if (event.getPlayer() != null) {  // Fires with null player on login. Don't pass these fires on to listeners.
+                listener.accept(event.getPlayer());
+            }
         });
     }
 
