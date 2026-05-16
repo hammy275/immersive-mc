e63b9ac7f6b1a8aed55d634f6ae6352780b81f1e
hammy275 <hammy275@gmail.com>
2026-05-16 14:58:32 -0400

    Another Forge Compile Fix for 26.1.x

--- forge/src/main/java/com/hammy275/immersivemc/forge/PlatformCommonImpl.java
+++ forge/src/main/java/com/hammy275/immersivemc/forge/PlatformCommonImpl.java
@@ -36,7 +36,7 @@ public class PlatformCommonImpl implements PlatformCommon {
     }
     @Override
     public boolean isModLoaded(String modId) {
-        return ModList.get().isLoaded(modId);
+        return ModList.isLoaded(modId);
     }
     @Override
     public File getConfigFolder() {
