881a89bc663406d870f102929900ff8736c3973d
hammy275 <hammy275@gmail.com>
2026-05-22 19:18:50 -0400

    ServerMixinProxy not ClientMixinProxy for skipIncrementDecrementChests (#644)

--- common/src/main/java/com/hammy275/immersivemc/client/ClientMixinProxy.java
+++ common/src/main/java/com/hammy275/immersivemc/client/ClientMixinProxy.java
@@ -24,11 +24,6 @@ import java.util.stream.StreamSupport;
 public class ClientMixinProxy {
 
     public static boolean pretendPlayerIsNotCrouching = false;
-    /**
-     * Needed since the decrement of openers to -1 is very bad, as when written as a count of number of openers then
-     * read back, it's read as an unsigned byte (255). Good to protect from bad values here anyway though.
-     */
-    public static boolean skipIncrementDecrementChests = false;
 
     public static boolean playerIsLocalPlayer(Player player) {
         return player == Minecraft.getInstance().player;
