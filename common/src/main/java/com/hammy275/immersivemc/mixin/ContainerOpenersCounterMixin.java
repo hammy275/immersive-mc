21f6b71147b041d079d9958e26efd2737ed24391
hammy275 <hammy275@gmail.com>
2026-05-22 19:18:50 -0400

    ServerMixinProxy not ClientMixinProxy for skipIncrementDecrementChests (#644)

--- common/src/main/java/com/hammy275/immersivemc/mixin/ContainerOpenersCounterMixin.java
+++ common/src/main/java/com/hammy275/immersivemc/mixin/ContainerOpenersCounterMixin.java
@@ -1,10 +1,11 @@
 package com.hammy275.immersivemc.mixin;
 
-import com.hammy275.immersivemc.client.ClientMixinProxy;
 import com.hammy275.immersivemc.server.ChestToOpenSet;
+import com.hammy275.immersivemc.server.ServerMixinProxy;
 import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.ContainerOpenersCounter;
@@ -16,7 +17,7 @@ public class ContainerOpenersCounterMixin {
 
     @WrapMethod(method = "incrementOpeners")
     private void immersiveMC$skipIncrementOpeners(Player player, Level level, BlockPos pos, BlockState state, Operation<Void> original) {
-        if (!ClientMixinProxy.skipIncrementDecrementChests) {
+        if (!ServerMixinProxy.skipIncrementDecrementChests || !(player instanceof ServerPlayer)) {
             original.call(player, level, pos, state);
         } else {
             ((ContainerOpenersCounter) (Object) this).recheckOpeners(level, pos, state);
@@ -25,7 +26,7 @@ public class ContainerOpenersCounterMixin {
 
     @WrapMethod(method = "decrementOpeners")
     private void immersiveMC$skipDecrementOpeners(Player player, Level level, BlockPos pos, BlockState state, Operation<Void> original) {
-        if (!ClientMixinProxy.skipIncrementDecrementChests) {
+        if (!ServerMixinProxy.skipIncrementDecrementChests || !(player instanceof ServerPlayer)) {
             original.call(player, level, pos, state);
         } else {
             ((ContainerOpenersCounter) (Object) this).recheckOpeners(level, pos, state);
--- common/src/main/java/com/hammy275/immersivemc/server/ServerMixinProxy.java
+++ common/src/main/java/com/hammy275/immersivemc/server/ServerMixinProxy.java
@@ -4,4 +4,9 @@ public class ServerMixinProxy {
 
     public static boolean pretendPlayerIsNotCrouching = false;
 
+    /**
+     * Needed since the decrement of openers to -1 is very bad, as when written as a count of number of openers then
+     * read back, it's read as an unsigned byte (255). Good to protect from bad values here anyway though.
+     */
+    public static boolean skipIncrementDecrementChests = false;
 }
