125d9dad35f137347e8938dc30f7c19639b7ffd9
hammy275 <hammy275@gmail.com>
2026-05-22 21:22:34 -0400

    Fix Bad Ender Chest Cast and Version Number Fix+Increment

--- common/src/main/java/com/hammy275/immersivemc/common/util/Util.java
+++ common/src/main/java/com/hammy275/immersivemc/common/util/Util.java
@@ -3,7 +3,6 @@ package com.hammy275.immersivemc.common.util;
 
 import com.hammy275.immersivemc.ImmersiveMC;
 import com.hammy275.immersivemc.Platform;
-import com.hammy275.immersivemc.PlatformCommon;
 import com.hammy275.immersivemc.api.client.immersive.Immersive;
 import com.hammy275.immersivemc.api.client.immersive.ImmersiveInfo;
 import com.hammy275.immersivemc.api.common.ImmersiveLogicHelpers;
@@ -44,12 +43,7 @@ import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.Vec3;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.List;
-import java.util.Optional;
-import java.util.Set;
+import java.util.*;
 
 public class Util {
 
@@ -334,14 +328,14 @@ public class Util {
     }
 
     @Nullable
-    public static ChestBlockEntity getOtherChest(ChestBlockEntity chest) {
+    public static ChestBlockEntity getOtherChest(BlockEntity chest) {
         return getOtherChest(chest, true);
     }
 
     @Nullable
-    protected static ChestBlockEntity getOtherChest(ChestBlockEntity chest, boolean checkOther) {
+    protected static ChestBlockEntity getOtherChest(BlockEntity chestIn, boolean checkOther) {
         // Gets the chest this one is connected to. Can be null.
-        if (chest == null) return null;
+        if (!(chestIn instanceof ChestBlockEntity chest)) return null;
         Direction otherDir = ChestBlock.getConnectedDirection(chest.getBlockState());
         BlockPos otherPos = chest.getBlockPos().relative(otherDir);
         if (chest.getLevel() != null && chest.getLevel().getBlockEntity(otherPos) instanceof ChestBlockEntity) {
