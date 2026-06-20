e23182dea2056689ff1c38ca3c5b95cae73c29ce
hammy275 <hammy275@gmail.com>
2026-06-19 20:48:14 -0400

    Fix Inconsistent Large Chest Syncing (#657)

--- common/src/main/java/com/hammy275/immersivemc/common/immersive/handler/ImmersiveHandlers.java
+++ common/src/main/java/com/hammy275/immersivemc/common/immersive/handler/ImmersiveHandlers.java
@@ -33,7 +33,7 @@ public class ImmersiveHandlers {
     public static final BlockBasedImmersiveHandler<?> barrelHandler = new BarrelHandler();
     public static final WorldStorageHandler<BeaconStorage> beaconHandler = new BeaconHandler();
     public static final BlockBasedImmersiveHandler<?> brewingStandHandler = new BrewingStandHandler();
-    public static final BlockBasedImmersiveHandler<ListOfItemsStorage> chestHandler = new ChestHandler();
+    public static final BlockBasedImmersiveHandler<ChestStorage> chestHandler = new ChestHandler();
     public static final WorldStorageHandler<CraftingTableStorage> craftingHandler = new CraftingHandler();
     public static final BlockBasedImmersiveHandler<NullStorage> doorHandler = new DoorHandler();
     public static final WorldStorageHandler<ETableStorage> enchantingTableHandler = new ETableHandler();
