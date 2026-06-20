7b6e79c9ac553efffa8c318450e15ad70741ea70
hammy275 <hammy275@gmail.com>
2026-06-19 20:48:14 -0400

    Fix Inconsistent Large Chest Syncing (#657)

---
+++ common/src/main/java/com/hammy275/immersivemc/common/immersive/storage/network/impl/ChestStorage.java
@@ -0,0 +1,41 @@
+package com.hammy275.immersivemc.common.immersive.storage.network.impl;
+
+import com.hammy275.immersivemc.api.common.immersive.NetworkStorage;
+import net.minecraft.core.BlockPos;
+import net.minecraft.network.RegistryFriendlyByteBuf;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ChestStorage implements NetworkStorage {
+
+    public final Map<BlockPos, ListOfItemsStorage> items = new HashMap<>();
+
+    public ChestStorage() {
+
+    }
+
+    public ChestStorage(BlockPos pos, ListOfItemsStorage items) {
+        this.items.put(pos, items);
+    }
+
+    @Override
+    public void encode(RegistryFriendlyByteBuf buffer) {
+        buffer.writeInt(items.size());
+        for (Map.Entry<BlockPos, ListOfItemsStorage> entry : items.entrySet()) {
+            buffer.writeBlockPos(entry.getKey());
+            entry.getValue().encode(buffer);
+        }
+    }
+
+    @Override
+    public void decode(RegistryFriendlyByteBuf buffer) {
+        int size = buffer.readInt();
+        for (int i = 0; i < size; i++) {
+            BlockPos pos = buffer.readBlockPos();
+            ListOfItemsStorage itemsStorage = new ListOfItemsStorage();
+            itemsStorage.decode(buffer);
+            items.put(pos, itemsStorage);
+        }
+    }
+}
